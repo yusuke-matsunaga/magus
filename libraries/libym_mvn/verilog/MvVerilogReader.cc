547ec45d043ea1f26515d101ed9811b87e6cf22f
Yusuke Matsunaga <yusuke_matsunaga@ieee.org>
2010-07-14 14:09:18 +0900

    作成中

--- libraries/libym_mvn/verilog/MvVerilogReader.cc
+++ libraries/libym_mvn/verilog/MvVerilogReader.cc
@@ -10,7 +10,9 @@
 #include "ym_mvn/MvVerilogReader.h"
 #include "ym_mvn/MvMgr.h"
 #include "ym_verilog/vl/VlModule.h"
+#include "ym_verilog/vl/VlPrimitive.h"
 #include "ym_verilog/vl/VlIODecl.h"
+#include "ym_verilog/vl/VlDecl.h"
 
 
 BEGIN_NAMESPACE_YM_MVN
@@ -78,7 +80,8 @@ MvVerilogReader::gen_network(MvMgr& mgr)
       }
     }
   }
-  
+
+  return true;
 }
 
 // @brief メッセージハンドラを付加する．
@@ -131,11 +134,11 @@ MvVerilogReader::gen_module(MvMgr& mgr,
   
   MvModule* module = mgr.new_module(vl_module->name(), np, ibw_array, obw_array);
 
-  // モジュールインスタンスノードの生成
-  vector<const VlModule*> vl_module_list;
-  if ( mVlMgr.find_module_list(vl_module, vl_module_list) ) {
-    for (vector<const VlModule*>::iterator p = vl_module_list.begin();
-	 p != vl_module_list.end(); ++ p) {
+  // モジュールインスタンスの生成
+  vector<const VlModule*> module_list;
+  if ( mVlMgr.find_module_list(vl_module, module_list) ) {
+    for (vector<const VlModule*>::iterator p = module_list.begin();
+	 p != module_list.end(); ++ p) {
       const VlModule* vl_module1 = *p;
       MvModule* module1 = gen_module(mgr, vl_module1);
       if ( module1 == NULL ) {
@@ -145,21 +148,80 @@ MvVerilogReader::gen_module(MvMgr& mgr,
     }
   }
 
-  // モジュール配列インスタンスノードの生成
+  // モジュール配列インスタンスの生成
   vector<const VlModuleArray*> modulearray_list;
   if ( mVlMgr.find_modulearray_list(vl_module, modulearray_list) ) {
     for (vector<const VlModuleArray*>::iterator p = modulearray_list.begin();
 	 p != modulearray_list.end(); ++ p) {
-      const VlModuleArray* module_array = *p;
-      ymuint32 n = module_array->elem_num();
-      for (ymuint32 i = 0; i < n; ++ i) {
-	const VlModule* vl_module1 = module_array->elem_by_offset(i);
+      const VlModuleArray* vl_module_array = *p;
+      ymuint n = vl_module_array->elem_num();
+      for (ymuint i = 0; i < n; ++ i) {
+	const VlModule* vl_module1 = vl_module_array->elem_by_offset(i);
 	MvModule* module1 = gen_module(mgr, vl_module1);
 	if ( module1 == NULL ) {
 	  return NULL;
 	}
 	MvNode* node1 = mgr.new_inst(module, module1);
       }
+    }
+  }
+
+  // プリミティブインスタンスの生成
+  vector<const VlPrimitive*> primitive_list;
+  if ( mVlMgr.find_primitive_list(vl_module, primitive_list) ) {
+    for (vector<const VlPrimitive*>::iterator p = primitive_list.begin();
+	 p != primitive_list.end(); ++ p) {
+      const VlPrimitive* vl_prim = *p;
+    }
+  }
+  // プリミティブ配列インスタンスの生成
+  vector<const VlPrimArray*> primarray_list;
+  if ( mVlMgr.find_primarray_list(vl_module, primarray_list) ) {
+    for (vector<const VlPrimArray*>::iterator p = primarray_list.begin();
+	 p != primarray_list.end(); ++ p) {
+      const VlPrimArray* vl_primarray = *p;
+      ymuint n = vl_primarray->elem_num();
+      for (ymuint i = 0; i < n; ++ i) {
+	const VlPrimitive* vl_prim = vl_primarray->elem_by_offset(i);
+      }
+    }
+  }
+
+  // ネットの生成
+  vector<const VlDecl*> net_list;
+  if ( mVlMgr.find_decl_list(vl_module, vpiNet, net_list) ) {
+    for (vector<const VlDecl*>::iterator p = net_list.begin();
+	 p != net_list.end(); ++ p) {
+      const VlDecl* vl_decl = *p;
+      // 仮の through ノードに対応させる．
+      ymuint bw = vl_decl->bit_size();
+      MvNode* node = mgr.new_through(module, bw);
+      
+    }
+  }
+
+  // ネット配列の生成
+  vector<const VlDecl*> netarray_list;
+  if ( mVlMgr.find_decl_list(vl_module, vpiNetArray, netarray_list) ) {
+    for (vector<const VlDecl*>::iterator p = netarray_list.begin();
+	 p != netarray_list.end(); ++ p) {
+      const VlDecl* vl_decl = *p;
+      ymuint d = vl_decl->dimension();
+#if 0
+      // 仮の through ノードに対応させる．
+      ymuint bw = vl_decl->bit_size();
+      MvNode* node = mgr.new_through(bw);
+#endif 
+    }
+  }
+
+  // 継続的代入文の生成
+  vector<const VlContAssign*> contassign_list;
+  if ( mVlMgr.find_contassign_list(vl_module, contassign_list) ) {
+    for (vector<const VlContAssign*>::iterator p = contassign_list.begin();
+	 p != contassign_list.end(); ++ p) {
+      const VlContAssign* vl_contassign = *p;
+      
     }
   }
   
