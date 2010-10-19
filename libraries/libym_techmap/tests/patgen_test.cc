a01d4e7cfe1ebd177be827463495b682bf3d9ef6
Yusuke Matsunaga <yusuke_matsunaga@ieee.org>
2010-10-20 08:09:47 +0900

    patgen_test 作成中．

--- libraries/libym_techmap/tests/patgen_test.cc
+++ libraries/libym_techmap/tests/patgen_test.cc
@@ -9,9 +9,16 @@
 
 #include "patgen/patgen_nsdef.h"
 #include "patgen/PatGen.h"
+#include "ym_lexp/LogExpr.h"
 
 
 BEGIN_NAMESPACE_YM_TECHMAP_PATGEN
+
+void
+dump_pat(PgHandle h)
+{
+
+}
 
 void
 test(const LogExpr& expr)
@@ -35,8 +42,19 @@ int
 main(int argc,
      char** argv)
 {
+  using nsYm::LogExpr;
+  using nsYm::nsTechmap::nsPatgen::test;
 
-  nsYm::nsTechmap::nsPatgen::test(expr);
+  LogExpr var0 = LogExpr::make_posiliteral(0);
+  LogExpr var1 = LogExpr::make_posiliteral(0);
+  LogExpr var2 = LogExpr::make_posiliteral(0);
+  LogExpr var3 = LogExpr::make_posiliteral(0);
+
+  LogExpr expr1 = var0 & var1 & var2 & var3;
+  test(expr1);
+
+  LogExpr expr2 = (var0 & var1) | (var2 & var3);
+  test(expr2);
 
   return 0;
 }
