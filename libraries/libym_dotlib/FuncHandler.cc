ccf5ca7ed62ec90285a7c6d1b23451b8ed15f18f
Yusuke Matsunaga <yusuke_matsunaga@ieee.org>
2011-05-11 04:38:27 +0900

    FuncHandler のテンプレートを追加

---
+++ libraries/libym_dotlib/FuncHandler.cc
@@ -0,0 +1,173 @@
+
+/// @file libym_dotlib/FuncHandler.cc
+/// @brief FuncHandler の実装ファイル
+/// @author Yusuke Matsunaga (松永 裕介)
+///
+/// Copyright (C) 2005-2011 Yusuke Matsunaga
+/// All rights reserved.
+
+
+#include "FuncHandler.h"
+#include "DotlibParserImpl.h"
+#include "DotlibMgrImpl.h"
+#include "GroupHandler.h"
+#include "DotlibNodeImpl.h"
+#include "ym_utils/MsgMgr.h"
+
+
+BEGIN_NAMESPACE_YM_DOTLIB
+
+//////////////////////////////////////////////////////////////////////
+// クラス FuncHandler
+//////////////////////////////////////////////////////////////////////
+
+// @brief コンストラクタ
+// @param[in] parent 親のハンドラ
+FuncHandler::FuncHandler(GroupHandler* parent) :
+  SimpleHandler(parent)
+{
+}
+
+// @brief デストラクタ
+FuncHandler::~FuncHandler()
+{
+}
+
+// @brief 値を読み込む処理
+// @return 値を表す DotlibNode を返す．
+// @note エラーが起きたら NULL を返す．
+// @note ここでは expression のパースを行う．
+DotlibNodeImpl*
+FuncHandler::read_value()
+{
+  tTokenType value_type = parser().read_token(mCurLoc, false);
+  if ( value_type != SYMBOL ) {
+    MsgMgr::put_msg(__FILE__, __LINE__,
+		    mCurLoc,
+		    kMsgError,
+		    "DOTLIB_PARSER",
+		    "String value is expected.");
+    return NULL;
+  }
+
+  mString = parser().cur_string();
+  mCurPos;
+
+  DotlibNodeImpl* expr = read_expr(EOF);
+
+  return expr;
+}
+
+// @brief primary を読み込む．
+DotlibNodeImpl*
+FuncHandler::read_primary()
+{
+  FileRegion loc;
+  tTokenType type = read_token(loc);
+  if ( type == LP ) {
+    return read_expr(RP);
+  }
+  if ( type == SYMBOL ) {
+    ShString name(parser().cur_string());
+    return mgr()->new_string(name, loc);
+  }
+  if ( type == INT_NUM ) {
+    return mgr()->new_int(parser().cur_int(), loc);
+  }
+
+  MsgMgr::put_msg(__FILE__, __LINE__,
+		  loc,
+		  kMsgError,
+		  "DOTLIB_PARSER",
+		  "Syntax error. number is expected.");
+  return NULL;
+}
+
+// @brief prudct を読み込む．
+DotlibNodeImpl*
+FuncHandler::read_product()
+{
+  DotlibNodeImpl* opr1 = read_primary();
+  if ( opr1 == NULL ) {
+    return NULL;
+  }
+
+  for ( ; ; ) {
+    FileRegion loc;
+    tTokenType type = read_token(loc);
+    if ( type == MULT || type == DIV ) {
+      DotlibNodeImpl* opr2 = read_primary();
+      if ( opr2 == NULL ) {
+	return NULL;
+      }
+      if ( type == MULT ) {
+	opr1 = mgr()->new_mult(opr1, opr2);
+      }
+      else {
+	opr1 = mgr()->new_div(opr1, opr2);
+      }
+    }
+    else {
+      // token を戻す．
+      mUngetType = type;
+      mUngetLoc = loc;
+      return opr1;
+    }
+  }
+}
+
+// @brief expression を読み込む．
+DotlibNodeImpl*
+FuncHandler::read_expr(tTokenType end_marker)
+{
+  DotlibNodeImpl* opr1 = read_product();
+  if ( opr1 == NULL ) {
+    return NULL;
+  }
+  for ( ; ; ) {
+    FileRegion loc;
+    tTokenType type = read_token(loc);
+    if ( type == end_marker ) {
+      return opr1;
+    }
+    if ( type == PLUS || type == MINUS ) {
+      DotlibNodeImpl* opr2 = read_product();
+      if ( opr2 == NULL ) {
+	return NULL;
+      }
+      if ( type == PLUS ) {
+	opr1 = mgr()->new_plus(opr1, opr2);
+      }
+      else {
+	opr1 = mgr()->new_minus(opr1, opr2);
+      }
+    }
+    else {
+      MsgMgr::put_msg(__FILE__, __LINE__,
+		      loc,
+		      kMsgError,
+		      "DOTLIB_PARSER",
+		      "Syntax error.");
+      return NULL;
+    }
+  }
+}
+
+// @brief トークンを読み込む．
+// @param[out] loc 対応するファイル上の位置情報を格納する変数
+tTokenType
+FuncHandler::read_token(FileRegion& loc)
+{
+  if ( mUngetType != ERROR ) {
+    tTokenType ans = mUngetType;
+    loc = mUngetLoc;
+    mUngetType = ERROR;
+    return ans;
+  }
+  else {
+
+    return parser().read_token(loc);
+  }
+}
+
+END_NAMESPACE_YM_DOTLIB
---
+++ libraries/libym_dotlib/FuncHandler.h
@@ -0,0 +1,97 @@
+#ifndef LIBYM_DOTLIB_FUNCHANDLER_H
+#define LIBYM_DOTLIB_FUNCHANDLER_H
+
+/// @file libym_dotlib/FuncHandler.h
+/// @brief FuncHandler のヘッダファイル
+/// @author Yusuke Matsunaga (松永 裕介)
+///
+/// Copyright (C) 2005-2011 Yusuke Matsunaga
+/// All rights reserved.
+
+
+#include "SimpleHandler.h"
+#include "ym_utils/FileRegion.h"
+
+
+BEGIN_NAMESPACE_YM_DOTLIB
+
+//////////////////////////////////////////////////////////////////////
+/// @class FuncHandler FuncHandler.h "FuncHandler.h"
+/// @brief function 属性の値をパーズする simple attribute 用のハンドラ
+//////////////////////////////////////////////////////////////////////
+class FuncHandler :
+  public SimpleHandler
+{
+public:
+
+  /// @brief コンストラクタ
+  /// @param[in] parent 親のハンドラ
+  FuncHandler(GroupHandler* parent);
+
+  /// @brief デストラクタ
+  virtual
+  ~FuncHandler();
+
+
+protected:
+  //////////////////////////////////////////////////////////////////////
+  // SimpleHandler の仮想関数
+  //////////////////////////////////////////////////////////////////////
+
+  /// @brief 値を読み込む処理
+  /// @return 値を表す DotlibNode を返す．
+  /// @note エラーが起きたら NULL を返す．
+  /// @note ここでは function 属性用の論理式のパースを行う．
+  virtual
+  DotlibNodeImpl*
+  read_value();
+
+
+private:
+  //////////////////////////////////////////////////////////////////////
+  // 内部で用いられる関数
+  //////////////////////////////////////////////////////////////////////
+
+  /// @brief primary を読み込む．
+  DotlibNodeImpl*
+  read_primary();
+
+  /// @brief prudct を読み込む．
+  DotlibNodeImpl*
+  read_product();
+
+  /// @brief expression を読み込む．
+  DotlibNodeImpl*
+  read_expr(tTokenType end_marker);
+
+  /// @brief トークンを読み込む．
+  /// @param[out] loc 対応するファイル上の位置情報を格納する変数
+  tTokenType
+  read_token(FileRegion& loc);
+
+
+private:
+  //////////////////////////////////////////////////////////////////////
+  // データメンバ
+  //////////////////////////////////////////////////////////////////////
+
+  // パーズ中の文字列
+  const char* mString;
+
+  // mString のファイル位置
+  FileRegion mCurLoc;
+
+  // 現在の読み出し位置
+  ymuint mCurPos;
+
+  // 読み戻したトークンの型
+  tTokenType mUngetType;
+
+  // 読み戻したトークンの位置
+  FileRegion mUngetLoc;
+
+};
+
+END_NAMESPACE_YM_DOTLIB
+
+#endif // LIBYM_DOTLIB_FUNCHANDLER_H
