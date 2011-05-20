1f523f46713af6d5d043b7ed4a8c980c13c94f8c
Yusuke Matsunaga <yusuke_matsunaga@ieee.org>
2011-05-20 16:12:52 +0900

    cell_mgr_test のコードを整理した．

--- libraries/libym_techmap/tests/cell_mgr_test.cc
+++ libraries/libym_techmap/tests/cell_mgr_test.cc
@@ -3,58 +3,90 @@
 /// @brief CellMgr のテストプログラム
 /// @author Yusuke Matsunaga (松永 裕介)
 ///
-/// Copyright (C) 2005-2010 Yusuke Matsunaga
+/// Copyright (C) 2005-2011 Yusuke Matsunaga
 /// All rights reserved.
 
 
-#include "ym_techmap/CellMap.h"
 #include "cellmap/CellMgr.h"
-//#include "cellmap/patgen/patgen_nsdef.h"
+#include "cellmap/patgen/PgFuncMgr.h"
 #include "ym_cell/CellMislibReader.h"
 #include "ym_cell/CellLibrary.h"
+
+
+BEGIN_NAMESPACE_YM_CELLMAP_PATGEN
+
+// セルライブラリのパタンを生成してダンプする．
+void
+dump_patdata(ostream& s,
+	     const CellLibrary& library)
+{
+  PgFuncMgr pgf_mgr;
+  pgf_mgr.set_library(&library);
+
+  pg_dump(s, pgf_mgr);
+}
+
+END_NAMESPACE_YM_CELLMAP_PATGEN
+
+
+BEGIN_NAMESPACE_YM_CELLMAP
+
+bool
+dump_load_test(const char* in_filename,
+	       const char* data_filename)
+{
+  using nsPatgen::dump_patdata;
+
+  CellMislibReader reader;
+  const CellLibrary* library = reader.read(in_filename);
+  if ( library == NULL ) {
+    cerr << in_filename << ": Error in reading library" << endl;
+    return false;
+  }
+
+  ofstream os;
+  os.open(data_filename, ios::binary);
+  if ( !os ) {
+    // エラー
+    cerr << "Could not create " << data_filename << endl;
+    return false;
+  }
+
+  dump_patdata(os, *library);
+
+  os.close();
+
+  CellMgr cell_mgr;
+  {
+    ifstream ifs;
+    ifs.open(data_filename, ios::binary);
+    if ( !ifs ) {
+      // エラー
+      cerr << "Could not open " << data_filename << endl;
+      return false;
+    }
+    cell_mgr.load_library(ifs);
+  }
+  dump(cout, cell_mgr);
+
+  return true;
+}
+
+END_NAMESPACE_YM_CELLMAP
 
 
 int
 main(int argc,
      char** argv)
 {
-  using namespace std;
-  using namespace nsYm;
-  using namespace nsYm::nsCellmap;
+  using nsYm::nsCellmap::dump_load_test;
 
   const char* filename = argv[1];
-  CellMislibReader reader;
-  const CellLibrary* library = reader.read(filename);
-  if ( library == NULL ) {
-    return 1;
-  }
-
   const char* datafile = "patdata.bin";
 
-  CellMap cell_map;
-  {
-    ofstream os;
-    os.open(datafile, ios::binary);
-    if ( !os ) {
-      // エラー
-      cerr << "Could not create " << datafile << endl;
-      return 2;
-    }
-
-    cell_map.dump_library(os, *library);
-    os.close();
+  if ( !dump_load_test(filename, datafile) ) {
+    return -1;
   }
 
-  CellMgr cell_mgr;
-  {
-    ifstream ifs;
-    ifs.open(datafile, ios::binary);
-    if ( !ifs ) {
-      // エラー
-      cerr << "Could not open " << datafile << endl;
-      return 3;
-    }
-    cell_mgr.load_library(ifs);
-  }
-  dump(cout, cell_mgr);
+  return 0;
 }
