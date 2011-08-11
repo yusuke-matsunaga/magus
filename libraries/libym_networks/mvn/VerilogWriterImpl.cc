ff10882a81aa919de5f4497ede20f26e9b185e18
Yusuke Matsunaga <yusuke_matsunaga@ieee.org>
2011-08-11 18:18:42 +0900

    シンプルな場合のポート記述を見やすくした．

--- libraries/libym_networks/mvn/VerilogWriterImpl.cc
+++ libraries/libym_networks/mvn/VerilogWriterImpl.cc
@@ -36,35 +36,203 @@
 
 BEGIN_NAMESPACE_YM_NETWORKS_MVN
 
-BEGIN_NONAMESPACE
+//////////////////////////////////////////////////////////////////////
+// クラス VerilogWriterImpl
+//////////////////////////////////////////////////////////////////////
 
-string
-node_name(const MvnNode* node)
+// @brief コンストラクタ
+VerilogWriterImpl::VerilogWriterImpl()
 {
-  ostringstream buf;
-  buf << "node" << node->id();
-  return buf.str();
+}
+
+// @brief デストラクタ
+VerilogWriterImpl::~VerilogWriterImpl()
+{
+}
+
+// @brief 内容を Verilog-HDL 形式で出力する
+// @param[in] s 出力先のストリーム
+// @param[in] mgr MvnMgr
+void
+VerilogWriterImpl::dump(ostream& s,
+			const MvnMgr& mgr)
+{
+  mNameArray.clear();
+  mNameArray.resize(mgr.max_node_id(), string());
+  ymuint n = mgr.max_module_id();
+  for (ymuint i = 0; i < n; ++ i) {
+    const MvnModule* module = mgr.module(i);
+    if ( module == NULL ) continue;
+
+    dump_module(s, module, mgr);
+  }
+}
+
+// @brief 内容を Verilog-HDL 形式で出力する
+// @param[in] s 出力先のストリーム
+// @param[in] mgr MvnMgr
+// @param[in] node_map ノードと Verilog 名の対応表
+void
+VerilogWriterImpl::dump(ostream& s,
+			const MvnMgr& mgr,
+			const MvnVlMap& node_map)
+{
+  dump(s, mgr);
+
+  ymuint node_num = mgr.max_node_id();
+  for (ymuint i = 0; i < node_num; ++ i) {
+    const MvnNode* node = mgr.node(i);
+    if ( node == NULL ) continue;
+
+    s << "// node" << node->id() << " : ";
+    if ( node_map.is_single_elem(i) ) {
+      const VlDecl* decl = node_map.get_single_elem(i);
+      assert_cond( decl != NULL, __FILE__, __LINE__);
+      s << decl->full_name();
+    }
+    else if ( node_map.is_array_elem(i) ) {
+      const VlDeclArray* declarray = node_map.get_array_elem(i);
+      assert_cond( declarray != NULL, __FILE__, __LINE__);
+      ymuint offset = node_map.get_array_offset(i);
+      ymuint d = declarray->dimension();
+      vector<int> index_array(d);
+      for (ymuint i = 0; i < d; ++ i) {
+	const VlRange* range = declarray->range(i);
+	ymuint n = range->size();
+	index_array[i] = offset % n;
+	offset /= n;
+      }
+      s << declarray->full_name();
+      for (ymuint i = 0; i < d; ++ i) {
+	s << "[" << index_array[d - i - 1] << "]";
+      }
+    }
+    s << endl;
+  }
 }
 
 void
-dump_port_ref(ostream& s,
-	      const MvnPortRef* port_ref)
+VerilogWriterImpl::dump_module(ostream& s,
+			       const MvnModule* module,
+			       const MvnMgr& mgr)
 {
-  s << node_name(port_ref->node());
-  if ( port_ref->has_bitselect() ) {
-    s << "[" << port_ref->bitpos() << "]";
+  s << "module " << module->name() << "(";
+
+  ymuint np = module->port_num();
+  const char* comma = "";
+  for (ymuint i = 0; i < np; ++ i) {
+    s << comma;
+    comma = ", ";
+    const MvnPort* port = module->port(i);
+    dump_port(s, port);
   }
-  else if ( port_ref->has_partselect() ) {
-    s << "[" << port_ref->msb() << ":" << port_ref->lsb() << "]";
+
+  s << ");" << endl;
+
+  ymuint ni = module->input_num();
+  for (ymuint i = 0; i < ni; ++ i) {
+    const MvnNode* node = module->input(i);
+    ymuint bw = node->output(0)->bit_width();
+    assert_cond( bw > 0, __FILE__, __LINE__);
+    if ( bw == 1 ) {
+      s << "  input ";
+    }
+    else {
+      s << "  input [" << bw - 1 << ":0] ";
+    }
+    s << node_name(node) << ";" << endl;
   }
+
+  ymuint no = module->output_num();
+  for (ymuint i = 0; i < no; ++ i) {
+    const MvnNode* node = module->output(i);
+    ymuint bw = node->input(0)->bit_width();
+    assert_cond( bw > 0, __FILE__, __LINE__);
+    if ( bw == 1 ) {
+      s << "  output ";
+    }
+    else {
+      s << "  output [" << bw - 1 << ":0] ";
+    }
+    s << node_name(node) << ";" << endl;
+  }
+
+  ymuint nio = module->inout_num();
+  for (ymuint i = 0; i < nio; ++ i) {
+    const MvnNode* node = module->inout(i);
+    ymuint bw = node->input(0)->bit_width();
+    assert_cond( bw > 0, __FILE__, __LINE__);
+    if ( bw == 1 ) {
+      s << "  inout ";
+    }
+    else {
+      s << "  inout [" << bw - 1 << ":0] ";
+    }
+    s << node_name(node) << ";" << endl;
+  }
+  s << endl;
+
+  const list<MvnNode*>& node_list = module->node_list();
+  for (list<MvnNode*>::const_iterator p = node_list.begin();
+       p != node_list.end(); ++ p) {
+    MvnNode* node = *p;
+    assert_cond( node->output_num() == 1, __FILE__, __LINE__);
+    ymuint bw = node->output(0)->bit_width();
+    if ( node->type() == MvnNode::kDff || node->type() == MvnNode::kLatch ) {
+      s << "  reg  ";
+      if ( bw > 1 ) {
+	s << "[" << bw - 1 << ":" << "0]";
+      }
+      s << " " << node_name(node) << ";" << endl;
+    }
+    else {
+      s << "  wire ";
+      if ( bw > 1 ) {
+	s << "[" << bw - 1 << ":" << "0]";
+      }
+      s << " " << node_name(node) << ";" << endl;
+    }
+  }
+  s << endl;
+
+  for (ymuint i = 0; i < ni; ++ i) {
+    const MvnNode* node = module->input(i);
+    dump_node(s, node);
+  }
+  for (ymuint i = 0; i < no; ++ i) {
+    const MvnNode* node = module->output(i);
+    dump_node(s, node);
+  }
+  for (ymuint i = 0; i < nio; ++ i) {
+    const MvnNode* node = module->inout(i);
+    dump_node(s, node);
+  }
+  for (list<MvnNode*>::const_iterator p = node_list.begin();
+       p != node_list.end(); ++ p) {
+    MvnNode* node = *p;
+    dump_node(s, node);
+  }
+
+  s << "endmodule" << endl
+    << endl;
 }
 
 void
-dump_port(ostream& s,
-	  const MvnPort* port)
+VerilogWriterImpl::dump_port(ostream& s,
+			     const MvnPort* port)
 {
-  s << "." << port->name() << "(";
+  string port_name = port->name();
   ymuint n = port->port_ref_num();
+  if ( n == 1 ) {
+    const MvnPortRef* port_ref = port->port_ref(0);
+    if ( !port_ref->has_bitselect() && !port_ref->has_partselect() ) {
+      s << port_name;
+      set_node_name(port_ref->node(), port_name);
+      return;
+    }
+  }
+
+  s << "." << port_name << "(";
   if ( n == 1 ) {
     const MvnPortRef* port_ref = port->port_ref(0);
     dump_port_ref(s, port_ref);
@@ -84,83 +252,21 @@ dump_port(ostream& s,
 }
 
 void
-dump_uop(ostream& s,
-	 const MvnNode* node,
-	 const char* opr_str)
+VerilogWriterImpl::dump_port_ref(ostream& s,
+				 const MvnPortRef* port_ref)
 {
-  ymuint ni = node->input_num();
-  assert_cond( ni == 1, __FILE__, __LINE__);
-  ymuint no = node->output_num();
-  assert_cond( no == 1, __FILE__, __LINE__);
-
-  const MvnInputPin* ipin = node->input(0);
-  const MvnOutputPin* src_pin = ipin->src_pin();
-  const MvnNode* src_node = src_pin->node();
-  s << "  assign " << node_name(node)
-    << " = " << opr_str << node_name(src_node)
-    << ";" << endl;
-}
-
-void
-dump_binop(ostream& s,
-	   const MvnNode* node,
-	   const char* opr_str,
-	   bool need_paren = false)
-{
-  ymuint ni = node->input_num();
-  assert_cond( ni == 2, __FILE__, __LINE__);
-  ymuint no = node->output_num();
-  assert_cond( no == 1, __FILE__, __LINE__);
-
-  const MvnInputPin* ipin0 = node->input(0);
-  const MvnOutputPin* src_pin0 = ipin0->src_pin();
-  const MvnNode* src_node0 = src_pin0->node();
-
-  const MvnInputPin* ipin1 = node->input(1);
-  const MvnOutputPin* src_pin1 = ipin1->src_pin();
-  const MvnNode* src_node1 = src_pin1->node();
-
-  const char* lp = "";
-  const char* rp = "";
-  if ( need_paren ) {
-    lp = "(";
-    rp = ")";
+  s << node_name(port_ref->node());
+  if ( port_ref->has_bitselect() ) {
+    s << "[" << port_ref->bitpos() << "]";
   }
-
-  s << "  assign " << node_name(node) << " = "
-    << lp << node_name(src_node0)
-    << " " << opr_str << " " << node_name(src_node1)
-    << rp << ";" << endl;
-}
-
-void
-dump_nop(ostream& s,
-	 const MvnNode* node,
-	 const char* opr_str)
-{
-  ymuint ni = node->input_num();
-  assert_cond( ni >= 2, __FILE__, __LINE__);
-  ymuint no = node->output_num();
-  assert_cond( no == 1, __FILE__, __LINE__);
-
-  const MvnInputPin* ipin0 = node->input(0);
-  const MvnOutputPin* src_pin0 = ipin0->src_pin();
-  const MvnNode* src_node0 = src_pin0->node();
-
-  s << "  assign " << node_name(node)
-    << " = " << node_name(src_node0);
-  for (ymuint i = 1; i < ni; ++ i) {
-    const MvnInputPin* ipin1 = node->input(i);
-    const MvnOutputPin* src_pin1 = ipin1->src_pin();
-    const MvnNode* src_node1 = src_pin1->node();
-    s << " " << opr_str << " " << node_name(src_node1);
+  else if ( port_ref->has_partselect() ) {
+    s << "[" << port_ref->msb() << ":" << port_ref->lsb() << "]";
   }
-  s << ";" << endl;
 }
 
 void
-dump_node(ostream& s,
-	  const MvnNode* node)
+VerilogWriterImpl::dump_node(ostream& s,
+			     const MvnNode* node)
 {
   switch ( node->type() ) {
   case MvnNode::kInput:
@@ -591,191 +697,97 @@ dump_node(ostream& s,
 }
 
 void
-dump_module(ostream& s,
-	    const MvnModule* module,
-	    const MvnMgr& mgr)
+VerilogWriterImpl::dump_uop(ostream& s,
+			    const MvnNode* node,
+			    const char* opr_str)
 {
-  s << "module " << module->name() << "(";
+  ymuint ni = node->input_num();
+  assert_cond( ni == 1, __FILE__, __LINE__);
+  ymuint no = node->output_num();
+  assert_cond( no == 1, __FILE__, __LINE__);
 
-  ymuint np = module->port_num();
-  const char* comma = "";
-  for (ymuint i = 0; i < np; ++ i) {
-    s << comma;
-    comma = ", ";
-    const MvnPort* port = module->port(i);
-    dump_port(s, port);
-  }
-
-  s << ");" << endl;
-
-  ymuint ni = module->input_num();
-  for (ymuint i = 0; i < ni; ++ i) {
-    const MvnNode* node = module->input(i);
-    ymuint bw = node->output(0)->bit_width();
-    assert_cond( bw > 0, __FILE__, __LINE__);
-    if ( bw == 1 ) {
-      s << "  input ";
-    }
-    else {
-      s << "  input [" << bw - 1 << ":0] ";
-    }
-    s << node_name(node) << ";" << endl;
-  }
-
-  ymuint no = module->output_num();
-  for (ymuint i = 0; i < no; ++ i) {
-    const MvnNode* node = module->output(i);
-    ymuint bw = node->input(0)->bit_width();
-    assert_cond( bw > 0, __FILE__, __LINE__);
-    if ( bw == 1 ) {
-      s << "  output ";
-    }
-    else {
-      s << "  output [" << bw - 1 << ":0] ";
-    }
-    s << node_name(node) << ";" << endl;
-  }
-
-  ymuint nio = module->inout_num();
-  for (ymuint i = 0; i < nio; ++ i) {
-    const MvnNode* node = module->inout(i);
-    ymuint bw = node->input(0)->bit_width();
-    assert_cond( bw > 0, __FILE__, __LINE__);
-    if ( bw == 1 ) {
-      s << "  inout ";
-    }
-    else {
-      s << "  inout [" << bw - 1 << ":0] ";
-    }
-    s << node_name(node) << ";" << endl;
-  }
-  s << endl;
-
-  const list<MvnNode*>& node_list = module->node_list();
-  for (list<MvnNode*>::const_iterator p = node_list.begin();
-       p != node_list.end(); ++ p) {
-    MvnNode* node = *p;
-    assert_cond( node->output_num() == 1, __FILE__, __LINE__);
-    ymuint bw = node->output(0)->bit_width();
-    if ( node->type() == MvnNode::kDff || node->type() == MvnNode::kLatch ) {
-      s << "  reg  ";
-      if ( bw > 1 ) {
-	s << "[" << bw - 1 << ":" << "0]";
-      }
-      s << " " << node_name(node) << ";" << endl;
-    }
-    else {
-      s << "  wire ";
-      if ( bw > 1 ) {
-	s << "[" << bw - 1 << ":" << "0]";
-      }
-      s << " " << node_name(node) << ";" << endl;
-    }
-  }
-  s << endl;
-
-  for (ymuint i = 0; i < ni; ++ i) {
-    const MvnNode* node = module->input(i);
-    dump_node(s, node);
-  }
-  for (ymuint i = 0; i < no; ++ i) {
-    const MvnNode* node = module->output(i);
-    dump_node(s, node);
-  }
-  for (ymuint i = 0; i < nio; ++ i) {
-    const MvnNode* node = module->inout(i);
-    dump_node(s, node);
-  }
-  for (list<MvnNode*>::const_iterator p = node_list.begin();
-       p != node_list.end(); ++ p) {
-    MvnNode* node = *p;
-    dump_node(s, node);
-  }
-
-  s << "endmodule" << endl
-    << endl;
+  const MvnInputPin* ipin = node->input(0);
+  const MvnOutputPin* src_pin = ipin->src_pin();
+  const MvnNode* src_node = src_pin->node();
+  s << "  assign " << node_name(node)
+    << " = " << opr_str << node_name(src_node)
+    << ";" << endl;
 }
 
-END_NONAMESPACE
-
-
-//////////////////////////////////////////////////////////////////////
-// クラス VerilogWriterImpl
-//////////////////////////////////////////////////////////////////////
-
-// @brief コンストラクタ
-VerilogWriterImpl::VerilogWriterImpl()
-{
-}
-
-// @brief デストラクタ
-VerilogWriterImpl::~VerilogWriterImpl()
-{
-}
-
-// @brief 内容を Verilog-HDL 形式で出力する
-// @param[in] s 出力先のストリーム
-// @param[in] mgr MvnMgr
 void
-VerilogWriterImpl::dump(ostream& s,
-			const MvnMgr& mgr)
+VerilogWriterImpl::dump_binop(ostream& s,
+			      const MvnNode* node,
+			      const char* opr_str,
+			      bool need_paren)
 {
-  ymuint n = mgr.max_module_id();
-  for (ymuint i = 0; i < n; ++ i) {
-    const MvnModule* module = mgr.module(i);
-    if ( module == NULL ) continue;
+  ymuint ni = node->input_num();
+  assert_cond( ni == 2, __FILE__, __LINE__);
+  ymuint no = node->output_num();
+  assert_cond( no == 1, __FILE__, __LINE__);
 
-    dump_module(s, module, mgr);
+  const MvnInputPin* ipin0 = node->input(0);
+  const MvnOutputPin* src_pin0 = ipin0->src_pin();
+  const MvnNode* src_node0 = src_pin0->node();
+
+  const MvnInputPin* ipin1 = node->input(1);
+  const MvnOutputPin* src_pin1 = ipin1->src_pin();
+  const MvnNode* src_node1 = src_pin1->node();
+
+  const char* lp = "";
+  const char* rp = "";
+  if ( need_paren ) {
+    lp = "(";
+    rp = ")";
   }
+
+  s << "  assign " << node_name(node) << " = "
+    << lp << node_name(src_node0)
+    << " " << opr_str << " " << node_name(src_node1)
+    << rp << ";" << endl;
 }
 
-// @brief 内容を Verilog-HDL 形式で出力する
-// @param[in] s 出力先のストリーム
-// @param[in] mgr MvnMgr
-// @param[in] node_map ノードと Verilog 名の対応表
 void
-VerilogWriterImpl::dump(ostream& s,
-			const MvnMgr& mgr,
-			const MvnVlMap& node_map)
+VerilogWriterImpl::dump_nop(ostream& s,
+			    const MvnNode* node,
+			    const char* opr_str)
 {
-  ymuint n = mgr.max_module_id();
-  for (ymuint i = 0; i < n; ++ i) {
-    const MvnModule* module = mgr.module(i);
-    if ( module == NULL ) continue;
+  ymuint ni = node->input_num();
+  assert_cond( ni >= 2, __FILE__, __LINE__);
+  ymuint no = node->output_num();
+  assert_cond( no == 1, __FILE__, __LINE__);
 
-    dump_module(s, module, mgr);
+  const MvnInputPin* ipin0 = node->input(0);
+  const MvnOutputPin* src_pin0 = ipin0->src_pin();
+  const MvnNode* src_node0 = src_pin0->node();
+
+  s << "  assign " << node_name(node)
+    << " = " << node_name(src_node0);
+  for (ymuint i = 1; i < ni; ++ i) {
+    const MvnInputPin* ipin1 = node->input(i);
+    const MvnOutputPin* src_pin1 = ipin1->src_pin();
+    const MvnNode* src_node1 = src_pin1->node();
+    s << " " << opr_str << " " << node_name(src_node1);
   }
+  s << ";" << endl;
+}
 
-  ymuint node_num = mgr.max_node_id();
-  for (ymuint i = 0; i < node_num; ++ i) {
-    const MvnNode* node = mgr.node(i);
-    if ( node == NULL ) continue;
-
-    s << "// node" << node->id() << " : ";
-    if ( node_map.is_single_elem(i) ) {
-      const VlDecl* decl = node_map.get_single_elem(i);
-      assert_cond( decl != NULL, __FILE__, __LINE__);
-      s << decl->full_name();
-    }
-    else if ( node_map.is_array_elem(i) ) {
-      const VlDeclArray* declarray = node_map.get_array_elem(i);
-      assert_cond( declarray != NULL, __FILE__, __LINE__);
-      ymuint offset = node_map.get_array_offset(i);
-      ymuint d = declarray->dimension();
-      vector<int> index_array(d);
-      for (ymuint i = 0; i < d; ++ i) {
-	const VlRange* range = declarray->range(i);
-	ymuint n = range->size();
-	index_array[i] = offset % n;
-	offset /= n;
-      }
-      s << declarray->full_name();
-      for (ymuint i = 0; i < d; ++ i) {
-	s << "[" << index_array[d - i - 1] << "]";
-      }
-    }
-    s << endl;
+string
+VerilogWriterImpl::node_name(const MvnNode* node)
+{
+  string name = mNameArray[node->id()];
+  if ( name == string() ) {
+    ostringstream buf;
+    buf << "node" << node->id();
+    name = buf.str();
   }
+  return name;
+}
+
+void
+VerilogWriterImpl::set_node_name(const MvnNode* node,
+				 const string& name)
+{
+  mNameArray[node->id()] = name;
 }
 
 END_NAMESPACE_YM_NETWORKS_MVN
--- libraries/libym_networks/mvn/VerilogWriterImpl.h
+++ libraries/libym_networks/mvn/VerilogWriterImpl.h
@@ -47,6 +47,61 @@ public:
        const MvnMgr& mgr,
        const MvnVlMap& node_map);
 
+
+private:
+  //////////////////////////////////////////////////////////////////////
+  // 内部で使われる関数
+  //////////////////////////////////////////////////////////////////////
+
+  void
+  dump_module(ostream& s,
+	      const MvnModule* module,
+	      const MvnMgr& mgr);
+
+  void
+  dump_port(ostream& s,
+	    const MvnPort* port);
+
+  void
+  dump_port_ref(ostream& s,
+		const MvnPortRef* port_ref);
+
+  void
+  dump_node(ostream& s,
+	    const MvnNode* node);
+
+  void
+  dump_uop(ostream& s,
+	   const MvnNode* node,
+	   const char* opr_str);
+
+  void
+  dump_binop(ostream& s,
+	     const MvnNode* node,
+	     const char* opr_str,
+	     bool need_paren = false);
+
+  void
+  dump_nop(ostream& s,
+	   const MvnNode* node,
+	   const char* opr_str);
+
+  string
+  node_name(const MvnNode* node);
+
+  void
+  set_node_name(const MvnNode* node,
+		const string& name);
+
+
+private:
+  //////////////////////////////////////////////////////////////////////
+  // データメンバ
+  //////////////////////////////////////////////////////////////////////
+
+  // ノードのID をキーにして名前を格納する配列
+  vector<string> mNameArray;
+
 };
 
 END_NAMESPACE_YM_NETWORKS_MVN
