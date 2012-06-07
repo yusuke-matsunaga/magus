a68b5e90c60f62ccf22ee54fa95d3577d9875bf9
Yusuke Matsunaga <yusuke_matsunaga@ieee.org>
2012-06-07 19:39:19 +0900

    tests/lrw: genpat 追加.　tests/genaig/genaig の XOR 対応版

---
+++ tests/lrw/genpat.cc
@@ -0,0 +1,32 @@
+
+/// @file genpat.cc
+/// @brief genpat のメインプログラム
+/// @author Yusuke Matsunaga (松永 裕介)
+///
+/// Copyright (C) 2005-2011 Yusuke Matsunaga
+/// All rights reserved.
+
+#include "GenPat.h"
+
+
+BEGIN_NAMESPACE_YM
+
+int
+genpat(int argc,
+       const char** argv)
+{
+  GenPat gp;
+
+  gp();
+
+  return 0;
+}
+
+END_NAMESPACE_YM
+
+int
+main(int argc,
+     const char** argv)
+{
+  nsYm::genpat(argc, argv);
+}
