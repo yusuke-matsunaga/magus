808a169811cf3b17477faf73970969476964b93d
Yusuke Matsunaga <yusuke_matsunaga@ieee.org>
2012-10-01 22:42:22 +0900

    libym_logic: pylib 追加中

---
+++ libraries/libym_logic/pylib/logicpy.h
@@ -0,0 +1,14 @@
+#ifndef LOGICPY_H
+#define LOGICPY_H
+
+/// @file logicpy.h
+/// @brief libym_logic の Python 用の拡張モジュールの定義ファイル
+/// @author Yusuke Matsunaga (松永 裕介)
+///
+/// Copyright (C) 2005-2012 Yusuke Matsunaga
+/// All rights reserved.
+
+
+#include "Python.h"
+#include "ym_logic/
+#endif // LOGICPY_H
