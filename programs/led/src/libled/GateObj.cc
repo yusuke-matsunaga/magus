8b0f0c32f0b288a4b8a8f8e2947cde267001e4ba
Yusuke Matsunaga <yusuke_matsunaga@ieee.org>
2013-02-12 00:24:42 +0900

    programs/led: GateObj 改造中

--- programs/led/src/libled/GateObj.cc
+++ programs/led/src/libled/GateObj.cc
@@ -13,18 +13,63 @@
 
 BEGIN_NAMESPACE_YM_LED
 
-// @brief 入力数を得る．
-ymuint
-GateObj::ipin_num() const
+// @brief コンストラクタ
+// @param[in] sym シンボル
+// @param[in] pos 位置
+inline
+GateObj::GateObj(const Symbol* sym,
+		 const QPoint& pos) :
+  mSymbol(sym),
+  mPosition(pos)
 {
-  return mSymbol->ipin_num();
 }
 
-// @brief 出力数を得る．
-ymuint
-GateObj::opin_num() const
+// @brief コンストラクタ
+// @param[in] sym シンボル
+// @param[in] x 位置のX座標
+// @param[in] y 位置のY座標
+inline
+GateObj::GateObj(const Symbol* sym,
+		 ymuint x,
+		 ymuint y) :
+  mSymbol(sym),
+  mPosition(x, y)
 {
-  return mSymbol->opin_num();
+}
+
+// @brief デストラクタ
+inline
+GateObj::~GateObj()
+{
+}
+
+// @brief 位置を設定する．
+// @param[in] pos 設定する位置
+inline
+void
+GateObj::set_position(const QPoint& pos)
+{
+  mPosition = pos;
+}
+
+// @brief 位置を設定する．
+// @param[in] x 位置のX座標
+// @param[in] y 位置のY座標
+inline
+void
+GateObj::set_position(ymuint x,
+		      ymuint y)
+{
+  mPosition.setX(x);
+  mPosition.setY(y);
+}
+
+// @brief 位置を取得する．
+inline
+QPoint
+GateObj::position() const
+{
+  return mPosition;
 }
 
 // @brief このゲートを囲む最小の矩形を表す左上と右下の点を得る．
@@ -36,6 +81,13 @@ GateObj::bounding_box() const
   return tmp;
 }
 
+// @brief 入力数を得る．
+ymuint
+GateObj::ipin_num() const
+{
+  return mSymbol->ipin_num();
+}
+
 // @brief pos 番目の入力ピン位置を得る．
 QPoint
 GateObj::ipin_location(ymuint pos) const
@@ -44,12 +96,33 @@ GateObj::ipin_location(ymuint pos) const
   return tmp += mPosition;
 }
 
+// @brief 出力数を得る．
+ymuint
+GateObj::opin_num() const
+{
+  return mSymbol->opin_num();
+}
+
 // @brief pos 番目の出力ピン位置を得る．
 QPoint
 GateObj::opin_location(ymuint pos) const
 {
   QPoint tmp = mSymbol->opin_location(pos);
   return tmp += mPosition;
+}
+
+// @brief 描画する．
+void
+GateObj::draw(QPainter& painter) const
+{
+  painter.save();
+
+  QMatrix matrix;
+  matrix.translate(mPosition.x(), mPosition.y());
+
+  mSymbol->draw(painter);
+
+  painter.restore();
 }
 
 END_NAMESPACE_YM_LED
