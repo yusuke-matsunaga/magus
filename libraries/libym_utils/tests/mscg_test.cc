823d0bec2ab06356beb40679d93f7cca58e0fdad
Yusuke Matsunaga <yusuke_matsunaga@ieee.org>
2013-04-29 00:32:53 +0900

    libym_utils/libym_utils/tests: mscg_test.cc, mspg_test.cc 改良

--- libraries/libym_utils/tests/mscg_test.cc
+++ libraries/libym_utils/tests/mscg_test.cc
@@ -16,15 +16,58 @@ int
 mscg_test(int argc,
 	  const char** argv)
 {
-  vector<ymuint> num_array(2);
-  num_array[0] = 3;
-  num_array[1] = 2;
-
-  for (MultiSetCombiGen mscg(num_array, 3); !mscg.is_end(); ++ mscg) {
-    for (ymuint i = 0; i < 3; ++ i) {
-      cout << " " << mscg(i);
+  try {
+    char buff[1024];
+    while ( cin.getline(buff, 1024, '\n') ) {
+      char* p = strtok(buff, " \t");
+      if ( !p ) {
+	cerr << "ERROR: ngrp must be specified" << endl;
+	return 2;
+      }
+      ymuint ng = atoi(p);
+      if ( ng == 0 ) {
+	cerr << "ERROR: ngrp == 0" << endl;
+	return 2;
+      }
+      vector<ymuint> num_array(ng);
+      for (ymuint g = 0; g < ng; ++ g) {
+	p = strtok(NULL, " \t");
+	if ( !p ) {
+	  cerr << "ERROR: n(" << g << ") must be specified" << endl;
+	  return 2;
+	}
+	ymuint n = atoi(p);
+	if ( n == 0 ) {
+	  cerr << "ERROR: n(" << g << ") == 0" << endl;
+	  return 2;
+	}
+	num_array[g] = n;
+      }
+      p = strtok(NULL, " \t");
+      if ( !p ) {
+	cerr << "ERROR: k must be specified" << endl;
+	return 2;
+      }
+      ymuint k = atoi(p);
+      ymuint nall = 0;
+      for (ymuint g = 0; g < ng; ++ g) {
+	nall += num_array[g];
+      }
+      if ( k > nall ) {
+	cerr << "ERROR: k is too big" << endl;
+	return 3;
+      }
+      for (MultiSetCombiGen mscg(num_array, k); !mscg.is_end(); ++ mscg) {
+	for (ymuint i = 0; i < k; ++ i) {
+	  cout << " " << mscg(i);
+	}
+	cout << endl;
+      }
     }
-    cout << endl;
+  }
+  catch ( AssertError a ) {
+    cerr << a << endl;
+    return -1;
   }
 
   return 0;
--- libraries/libym_utils/tests/mspg_test.cc
+++ libraries/libym_utils/tests/mspg_test.cc
@@ -16,15 +16,58 @@ int
 mspg_test(int argc,
 	  const char** argv)
 {
-  vector<ymuint> num_array(2);
-  num_array[0] = 3;
-  num_array[1] = 2;
-
-  for (MultiSetPermGen mspg(num_array, 5); !mspg.is_end(); ++ mspg) {
-    for (ymuint i = 0; i < 5; ++ i) {
-      cout << " " << mspg(i);
+  try {
+    char buff[1024];
+    while ( cin.getline(buff, 1024, '\n') ) {
+      char* p = strtok(buff, " \t");
+      if ( !p ) {
+	cerr << "ERROR: ngrp must be specified" << endl;
+	return 2;
+      }
+      ymuint ng = atoi(p);
+      if ( ng == 0 ) {
+	cerr << "ERROR: ngrp == 0" << endl;
+	return 2;
+      }
+      vector<ymuint> num_array(ng);
+      for (ymuint g = 0; g < ng; ++ g) {
+	p = strtok(NULL, " \t");
+	if ( !p ) {
+	  cerr << "ERROR: n(" << g << ") must be specified" << endl;
+	  return 2;
+	}
+	ymuint n = atoi(p);
+	if ( n == 0 ) {
+	  cerr << "ERROR: n(" << g << ") == 0" << endl;
+	  return 2;
+	}
+	num_array[g] = n;
+      }
+      p = strtok(NULL, " \t");
+      if ( !p ) {
+	cerr << "ERROR: k must be specified" << endl;
+	return 2;
+      }
+      ymuint k = atoi(p);
+      ymuint nall = 0;
+      for (ymuint g = 0; g < ng; ++ g) {
+	nall += num_array[g];
+      }
+      if ( k > nall ) {
+	cerr << "ERROR: k is too big" << endl;
+	return 3;
+      }
+      for (MultiSetPermGen mspg(num_array, k); !mspg.is_end(); ++ mspg) {
+	for (ymuint i = 0; i < k; ++ i) {
+	  cout << " " << mspg(i);
+	}
+	cout << endl;
+      }
     }
-    cout << endl;
+  }
+  catch ( AssertError a ) {
+    cerr << a << endl;
+    return -1;
   }
 
   return 0;
