10f3c2e275d97c31a4da7e0508c6498e63fc80f5
Yusuke Matsunaga <yusuke_matsunaga@ieee.org>
2013-06-06 23:54:19 +0900

    libym_logic/bdd/base: Apply 演算で片方がリテラル関数の時の処理を追加

--- libraries/libym_logic/bdd/base/AndOp.cc
+++ libraries/libym_logic/bdd/base/AndOp.cc
@@ -77,20 +77,59 @@ AndOp::apply_step(BddEdge f,
     g = tmp;
   }
 
+  tPol f_pol = f.pol();
+  tPol g_pol = g.pol();
+  BddNode* f_vp = f.get_node();
+  BddNode* g_vp = g.get_node();
+  ymuint f_level = f_vp->level();
+  ymuint g_level = g_vp->level();
+  ymuint level = f_level;
+  if ( level > g_level ) {
+    level = g_level;
+  }
+
+  BddEdge f_0, f_1;
+  split1(level, f_level, f, f_vp, f_pol, f_0, f_1);
+
+  BddEdge g_0, g_1;
+  split1(level, g_level, g, g_vp, g_pol, g_0, g_1);
+
+  // 片方が最上位のリテラル関数の場合の特別な処理
+  if ( f_0.is_zero() && f_1.is_one() ) {
+    // f が肯定のリテラルの場合
+    // f_0 と f_1 が異なるということは f_level == level
+    return new_node(f_level, BddEdge::make_zero(), g_1);
+  }
+  if ( f_0.is_one() && f_1.is_zero() ) {
+    // f が否定のリテラルの場合
+    // f_0 と f_1 が異なるということは f_level == level
+    return new_node(f_level, g_0, BddEdge::make_zero());
+  }
+  if ( g_0.is_zero() && g_1.is_one() ) {
+    // g が肯定のリテラルの場合
+    // g_0 と g_1 が異なるということは f_level == level
+    return new_node(g_level, BddEdge::make_zero(), f_1);
+  }
+  if ( g_0.is_one() && g_1.is_zero() ) {
+    // g が否定のリテラルの場合
+    // g_0 と g_1 が異なるということは f_level == level
+    return new_node(g_level, f_0, BddEdge::make_zero());
+  }
+
   BddEdge result = get(f, g);
   if ( result.is_error() ) {
     // 演算結果テーブルには登録されていない
-    BddEdge f_0, f_1;
-    BddEdge g_0, g_1;
-    ymuint level = split(f, g, f_0, f_1, g_0, g_1);
+
     BddEdge r_0 = apply_step(f_0, g_0);
     if ( r_0.is_overflow() ) {
       return BddEdge::make_overflow();
     }
+
     BddEdge r_1 = apply_step(f_1, g_1);
     if ( r_1.is_overflow() ) {
       return BddEdge::make_overflow();
     }
+
     result = new_node(level, r_0, r_1);
     put(f, g, result);
   }
--- libraries/libym_logic/bdd/base/XorOp.cc
+++ libraries/libym_logic/bdd/base/XorOp.cc
@@ -87,22 +87,58 @@ XorOp::apply_step(BddEdge f,
   f.normalize();
   g.normalize();
 
-  BddEdge result = get(f, g);
-  if ( result.is_error() ) {
-    // 演算結果テーブルには登録されていない
-    BddEdge f_0, f_1;
-    BddEdge g_0, g_1;
-    ymuint level = split(f, g, f_0, f_1, g_0, g_1);
-    BddEdge r_0 = apply_step(f_0, g_0);
-    if ( r_0.is_overflow() ) {
-      return BddEdge::make_overflow();
+  BddNode* f_vp = f.get_node();
+  BddNode* g_vp = g.get_node();
+  ymuint f_level = f_vp->level();
+  ymuint g_level = g_vp->level();
+  ymuint level = f_level;
+  if ( level > g_level ) {
+    level = g_level;
+  }
+
+  BddEdge f_0, f_1;
+  split1(level, f_level, f, f_vp, f_pol, f_0, f_1);
+
+  BddEdge g_0, g_1;
+  split1(level, g_level, g, g_vp, g_pol, g_0, g_1);
+
+  BddEdge result;
+  if ( f_0.is_zero() && f_1.is_one() ) {
+    // f が肯定のリテラルで最上位のレベルの場合
+    // f_0 と f_1 が異なっているということは f_level == level である．
+    result = new_node(level, ~g_0, g_1);
+  }
+  else if ( f_0.is_one() && f_1.is_zero() ) {
+    // f が否定のリテラルで最上位のレベルの場合
+    // f_0 と f_1 が異なっているということは f_level == level である．
+    result = new_node(level, g_0, ~g_1);
+  }
+  else if ( g_0.is_zero() && g_1.is_one() ) {
+    // g が肯定のリテラルで最上位のレベルの場合
+    // g_0 と g_1 が異なっているということは g_level == level である．
+    result = new_node(level, ~f_0, f_1);
+  }
+  else if ( g_0.is_one() && g_1.is_zero() ) {
+    // g が否定のリテラルで最上位のレベルの場合
+    // g_0 と g_1 が異なっているということは g_level == level である．
+    result = new_node(level, f_0, ~f_1);
+  }
+  else {
+    // 演算結果テーブルを探す．
+    result = get(f, g);
+    if ( result.is_error() ) {
+      // 演算結果テーブルには登録されていない
+      BddEdge r_0 = apply_step(f_0, g_0);
+      if ( r_0.is_overflow() ) {
+	return BddEdge::make_overflow();
+      }
+      BddEdge r_1 = apply_step(f_1, g_1);
+      if ( r_1.is_overflow() ) {
+	return BddEdge::make_overflow();
+      }
+      result = new_node(level, r_0, r_1);
+      put(f, g, result);
     }
-    BddEdge r_1 = apply_step(f_1, g_1);
-    if ( r_1.is_overflow() ) {
-      return BddEdge::make_overflow();
-    }
-    result = new_node(level, r_0, r_1);
-    put(f, g, result);
   }
 
   return BddEdge(result, ans_pol);
