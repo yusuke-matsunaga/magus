76b32e02a37ff978998d18307582ee47b46994d0
Yusuke Matsunaga <yusuke_matsunaga@ieee.org>
2013-08-19 15:30:25 +0900

    libym_utils/tests/z_test2 作成中

---
+++ libraries/libym_utils/tests/z_test2.cc
@@ -0,0 +1,42 @@
+
+/// @file z_state2.cc
+/// @brief ZState のテストプログラム
+/// @author Yusuke Matsunaga (松永 裕介)
+///
+/// Copyright (C) 2013 Yusuke Matsunaga
+/// All rights reserved.
+
+
+#include <stdio.h>
+#include "zopen.h"
+#include "../ZState.h"
+
+
+BEGIN_NAMESPACE_YM
+
+int
+z_test(int argc,
+       const char** argv)
+{
+  FILE* fp = zopen(argv[1], "r", 0);
+
+  for ( ; ; ) {
+    ymuint8 buff[4096];
+    char* tmp = fgets(reinterpret_cast<char*>(buff), sizeof(buff), fp);
+    cout << tmp;
+  }
+
+  return 0;
+}
+
+END_NAMESPACE_YM
+
+
+int
+main(int argc,
+     const char** argv)
+{
+  using nsYm::z_test;
+
+  return z_test(argc, argv);
+}
