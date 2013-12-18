32139d2b8a3c6d28724ecd8150461db9547e4365
Yusuke Matsunaga <yusuke_matsunaga@ieee.org>
2013-12-18 15:04:42 +0900

    libym_utils/tests/CppUTest: BtgMatchTest を追加

--- libraries/libym_utils/tests/CppUTest/ym_utils_CppUTest.h
+++ libraries/libym_utils/tests/CppUTest/ym_utils_CppUTest.h
@@ -23,5 +23,6 @@ IMPORT_TEST_GROUP(libym_utils_PermGenTestGroup);
 IMPORT_TEST_GROUP(libym_utils_SimpleAllocTestGroup);
 IMPORT_TEST_GROUP(libym_utils_UnitAllocTestGroup);
 IMPORT_TEST_GROUP(libym_utils_HeapTreeTestGroup);
+IMPORT_TEST_GROUP(libym_utils_BtgMatchTestGroup);
 
 #endif // YM_UTILS_CPPUTEST_H
