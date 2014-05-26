2f399ad9c9be2f2fe952ea3cc9538102ec470e0f
Yusuke Matsunaga <yusuke_matsunaga@ieee.org>
2014-05-26 18:57:22 +0900

    satpg/src/dtpg: SatEngineSingle2 を追加
    中身は SatEngineSingle そのもの

---
+++ programs/satpg/src/dtpg/SatEngineSingle2.cc
@@ -0,0 +1,134 @@
+
+/// @file SatEngineSingle2.cc
+/// @brief SatEngineSingle2 の実装ファイル
+/// @author Yusuke Matsunaga (松永 裕介)
+///
+/// Copyright (C) 2005-2010, 2012-2014 Yusuke Matsunaga
+/// All rights reserved.
+
+
+#include "SatEngineSingle2.h"
+
+#include "DetectOp.h"
+#include "UntestOp.h"
+#include "DtpgStats.h"
+#include "TpgNode.h"
+#include "TpgFault.h"
+#include "BackTracer.h"
+#include "LitMap.h"
+#include "logic/SatSolver.h"
+#include "logic/SatStats.h"
+
+
+BEGIN_NAMESPACE_YM_SATPG
+
+// @brief コンストラクタ
+SatEngineSingle2::SatEngineSingle2()
+{
+}
+
+// @brief デストラクタ
+SatEngineSingle2::~SatEngineSingle2()
+{
+}
+
+// @brief テストパタン生成を行なう．
+// @param[in] flist 故障リスト
+// @param[in] max_id ノード番号の最大値 + 1
+void
+SatEngineSingle2::run(TpgFault* fault,
+		      TpgNode* fnode,
+		      int fval,
+		      ymuint max_id,
+		      BackTracer& bt,
+		      DetectOp& dop,
+		      UntestOp& uop)
+{
+  cnf_begin();
+
+  SatSolver solver(sat_type(), sat_option(), sat_outp());
+
+  bt.set_max_id(max_id);
+
+  mark_region(solver, vector<TpgNode*>(1, fnode), max_id);
+
+
+  //////////////////////////////////////////////////////////////////////
+  // 正常回路の CNF を生成
+  //////////////////////////////////////////////////////////////////////
+  for (ymuint i = 0; i < tfo_tfi_size(); ++ i) {
+    TpgNode* node = tfo_tfi_node(i);
+    make_node_cnf(solver, node, GvarLitMap(node));
+  }
+
+  //////////////////////////////////////////////////////////////////////
+  // 故障回路の CNF を生成
+  //////////////////////////////////////////////////////////////////////
+  for (ymuint i = 0; i < tfo_size(); ++ i) {
+    TpgNode* node = tfo_tfi_node(i);
+
+    Literal glit(node->gvar(), false);
+    Literal flit(node->fvar(), false);
+    Literal dlit(node->dvar(), false);
+
+    // XOR(glit, flit, dlit) を追加する．
+    // 要するに正常回路と故障回路で異なっているとき dlit が 1 となる．
+    solver.add_clause(~glit, ~flit, ~dlit);
+    solver.add_clause(~glit,  flit,  dlit);
+    solver.add_clause( glit, ~flit,  dlit);
+    solver.add_clause( glit,  flit, ~dlit);
+
+    if ( node != fnode ) {
+      // 故障回路のゲートの入出力関係を表すCNFを作る．
+      make_node_cnf(solver, node, FvarLitMap(node));
+
+      // 出力の dlit が1になる条件を作る．
+      // - 入力の dlit のいずれかが 1
+      ymuint ni = node->fanin_num();
+      tmp_lits_begin(ni + 1);
+      tmp_lits_add(~dlit);
+      for (ymuint j = 0; j < ni; ++ j) {
+	TpgNode* inode = node->fanin(j);
+	if ( inode->has_fvar() ) {
+	  tmp_lits_add(Literal(inode->dvar(), false));
+	}
+      }
+      tmp_lits_end(solver);
+    }
+  }
+
+
+  //////////////////////////////////////////////////////////////////////
+  // 故障の検出条件
+  //////////////////////////////////////////////////////////////////////
+  tmp_lits_begin(output_list().size());
+  for (vector<TpgNode*>::const_iterator p = output_list().begin();
+       p != output_list().end(); ++ p) {
+    TpgNode* node = *p;
+    Literal dlit(node->dvar(), false);
+    tmp_lits_add(dlit);
+  }
+  tmp_lits_end(solver);
+
+  cnf_end();
+
+  // 故障に対するテスト生成を行なう．
+  tmp_lits_begin();
+
+  // dominator ノードの dvar は1でなければならない．
+  for (TpgNode* node = fnode; node != NULL; node = node->imm_dom()) {
+    Literal dlit(node->dvar(), false);
+    tmp_lits_add(dlit);
+  }
+
+  // fnode の dlit を1と仮定しているので
+  // ここでは fvar か gvar のどちらかを仮定すればよい．
+  bool inv = (fval == 0);
+  tmp_lits_add(Literal(fnode->fvar(), inv));
+
+  solve(solver, fault, bt, dop, uop);
+
+  clear_node_mark();
+}
+
+END_NAMESPACE_YM_SATPG
---
+++ programs/satpg/src/dtpg/SatEngineSingle2.h
@@ -0,0 +1,76 @@
+#ifndef SATENGINESINGLE2_H
+#define SATENGINESINGLE2_H
+
+/// @file SatEngineSingle2.h
+/// @brief SatEngineSingle2 のヘッダファイル
+///
+/// @author Yusuke Matsunaga (松永 裕介)
+///
+/// Copyright (C) 2005-2010, 2012-2014 Yusuke Matsunaga
+/// All rights reserved.
+
+
+#include "SatEngine.h"
+
+
+BEGIN_NAMESPACE_YM_SATPG
+
+//////////////////////////////////////////////////////////////////////
+/// @class SatEngineSingle2 SatEngineSingle2.h "SatEngineSingle2.h"
+/// @brief 1つの故障を対象とした CNF を生成する SatEngine
+//////////////////////////////////////////////////////////////////////
+class SatEngineSingle2 :
+  public SatEngine
+{
+public:
+
+  /// @brief コンストラクタ
+  SatEngineSingle2();
+
+  /// @brief デストラクタ
+  virtual
+  ~SatEngineSingle2();
+
+
+public:
+  //////////////////////////////////////////////////////////////////////
+  // 外部インターフェイス
+  //////////////////////////////////////////////////////////////////////
+
+  /// @brief テスト生成を行なう．
+  /// @param[in] f_tgt 対象の故障
+  /// @param[in] max_id ノード番号の最大値 + 1
+  /// @param[in] bt バックトレーサー
+  /// @param[in] dop パタンが求められた時に実行されるファンクタ
+  /// @param[in] uop 検出不能と判定された時に実行されるファンクタ
+  void
+  run(TpgFault* f_tgt,
+      TpgNode* fnode,
+      int val,
+      ymuint max_id,
+      BackTracer& bt,
+      DetectOp& dop,
+      UntestOp& uop);
+
+
+private:
+  //////////////////////////////////////////////////////////////////////
+  // 内部で用いられる下請け関数
+  //////////////////////////////////////////////////////////////////////
+
+
+private:
+  //////////////////////////////////////////////////////////////////////
+  // データメンバ
+  //////////////////////////////////////////////////////////////////////
+
+};
+
+
+//////////////////////////////////////////////////////////////////////
+// インライン関数の定義
+//////////////////////////////////////////////////////////////////////
+
+END_NAMESPACE_YM_SATPG
+
+#endif // SATENGINESINGLE2_H
