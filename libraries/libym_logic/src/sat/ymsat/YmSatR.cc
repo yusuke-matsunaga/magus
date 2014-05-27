ed610da1d299ea1f70cff6bcfef4d73de66d80bc
Yusuke Matsunaga <yusuke_matsunaga@ieee.org>
2014-05-27 19:32:27 +0900

    libym_logic/ymsat: YmSatR の add_clause() の変種が未実装だった．

--- libraries/libym_logic/src/sat/ymsat/YmSatR.cc
+++ libraries/libym_logic/src/sat/ymsat/YmSatR.cc
@@ -52,17 +52,103 @@ YmSatR::add_clause(const vector<Literal>& lits)
   for (vector<Literal>::const_iterator p = lits.begin();
        p != lits.end(); ++ p) {
     Literal l = *p;
-    mOut << " " << l.varid();
-    if ( l.is_positive() ) {
-      mOut << "P";
-    }
-    else {
-      mOut << "N";
-    }
+    put_lit(l);
   }
   mOut << endl;
 
   YmSat::add_clause(lits);
+}
+
+// @brief 節を追加する．
+// @param[in] lit_num リテラル数
+// @param[in] lits リテラルの配列
+void
+YmSatR::add_clause(ymuint lit_num,
+		   const Literal* lits)
+{
+  mOut << "A";
+  for (ymuint i = 0; i < lit_num; ++ i) {
+    Literal l = lits[i];
+    put_lit(l);
+  }
+  mOut << endl;
+
+  YmSat::add_clause(lit_num, lits);
+}
+
+// @brief 1項の節(リテラル)を追加する．
+void
+YmSatR::add_clause(Literal lit1)
+{
+  mOut << "A";
+  put_lit(lit1);
+  mOut << endl;
+
+  YmSat::add_clause(lit1);
+}
+
+// @brief 2項の節を追加する．
+void
+YmSatR::add_clause(Literal lit1,
+		   Literal lit2)
+{
+  mOut << "A";
+  put_lit(lit1);
+  put_lit(lit2);
+  mOut << endl;
+
+  YmSat::add_clause(lit1, lit2);
+}
+
+// @brief 3項の節を追加する．
+void
+YmSatR::add_clause(Literal lit1,
+		   Literal lit2,
+		   Literal lit3)
+{
+  mOut << "A";
+  put_lit(lit1);
+  put_lit(lit2);
+  put_lit(lit3);
+  mOut << endl;
+
+  YmSat::add_clause(lit1, lit2, lit3);
+}
+
+// @brief 4項の節を追加する．
+void
+YmSatR::add_clause(Literal lit1,
+		   Literal lit2,
+		   Literal lit3,
+		   Literal lit4)
+{
+  mOut << "A";
+  put_lit(lit1);
+  put_lit(lit2);
+  put_lit(lit3);
+  put_lit(lit4);
+  mOut << endl;
+
+  YmSat::add_clause(lit1, lit2, lit3, lit4);
+}
+
+// @brief 5項の節を追加する．
+void
+YmSatR::add_clause(Literal lit1,
+		   Literal lit2,
+		   Literal lit3,
+		   Literal lit4,
+		   Literal lit5)
+{
+  mOut << "A";
+  put_lit(lit1);
+  put_lit(lit2);
+  put_lit(lit3);
+  put_lit(lit4);
+  put_lit(lit5);
+  mOut << endl;
+
+  YmSat::add_clause(lit1, lit2, lit3, lit4, lit5);
 }
 
 // @brief SAT 問題を解く．
@@ -80,13 +166,7 @@ YmSatR::solve(const vector<Literal>& assumptions,
   for (vector<Literal>::const_iterator p = assumptions.begin();
        p != assumptions.end(); ++ p) {
     Literal l = *p;
-    mOut << " " << l.varid();
-    if ( l.is_positive() ) {
-      mOut << "P";
-    }
-    else {
-      mOut << "N";
-    }
+    put_lit(l);
   }
   mOut << endl;
 
@@ -95,6 +175,19 @@ YmSatR::solve(const vector<Literal>& assumptions,
   mOut << "# " << ans << endl;
 
   return ans;
+}
+
+// @brief リテラルを出力する．
+void
+YmSatR::put_lit(Literal lit)
+{
+  mOut << " " << lit.varid();
+  if ( lit.is_positive() ) {
+    mOut << "P";
+  }
+  else {
+    mOut << "N";
+  }
 }
 
 END_NAMESPACE_YM_SAT
