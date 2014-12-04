1d724f173bb5284427db52726361b5851b015117
Yusuke Matsunaga <yusuke_matsunaga@ieee.org>
2014-12-04 11:25:06 +0900

    ﻿libym_gds: GdsParser の非bison化進行中

--- libraries/libym_gds/include/gds/GdsParser.h
+++ libraries/libym_gds/include/gds/GdsParser.h
@@ -14,13 +14,6 @@
 #include "utils/SimpleAlloc.h"
 
 
-BEGIN_NAMESPACE_YM_GDS_PARSER
-
-// かなりかっこわるい & 危ない使い方．
-union YYSTYPE;
-
-END_NAMESPACE_YM_GDS_PARSER
-
 BEGIN_NAMESPACE_YM_GDS
 
 //////////////////////////////////////////////////////////////////////
@@ -51,169 +44,82 @@ public:
   parse(const string& filename);
 
 
-public:
+private:
   //////////////////////////////////////////////////////////////////////
-  // gds_grammer.yy で用いられる関数
+  // 内部で用いられる関数
   //////////////////////////////////////////////////////////////////////
 
-  /// @brief GdsData の生成
-  /// @param[in] version バージョン番号
-  /// @param[in] date 2つの日時の配列
-  /// @param[in] libdirsize LIBDIRSIZE の値
-  /// @param[in] srfname SRFNAME の値
-  /// @param[in] acl LIBSECURE の値
-  /// @param[in] libname LIBNAME の値
-  /// @param[in] reflibs REFLIBS の値
-  /// @param[in] fonts FONTS の値
-  /// @param[in] attrtable ATTRTABLE の値
-  /// @param[in] generations GENERATIONS の値
-  /// @param[in] units UNITS の値
-  void
-  new_header(ymint16 version,
-	     GdsDate* date,
-	     ymint16 libdirsize,
-	     GdsString* srfname,
-	     GdsACL* acl,
-	     GdsString* libname,
-	     GdsString* reflibs,
-	     GdsString* fonts,
-	     GdsString* attrtable,
-	     ymint16 generations,
-	     GdsUnits* units);
+  /// @brief HEADER の読み込み
+  ///
+  /// エラーが起きたら false を返す．
+  /// 結果は mCurData に格納される．
+  /// mCurStruct を NULL に初期化する．
+  bool
+  read_header();
 
-  /// @brief フォーマットタイプの設定
-  /// @param[in] type フォーマットタイプ
-  void
-  set_format(ymint16 type);
+  /// @brief BGNSTR 以降の読み込み
+  ///
+  /// エラーが起きたら false を返す．
+  /// 結果は mCurStruct に格納される．
+  /// mCurElement を NULL に初期化する．
+  bool
+  read_structure();
 
-  /// @brief マスクの追加
-  /// @param[in] mask マスク
-  void
-  add_mask(GdsString* mask);
+  /// @brief BOUNDARY 以降の読み込み
+  ///
+  /// エラーが起きたら false を返す．
+  /// 結果は mCurElement に格納される．
+  bool
+  read_boundary();
 
-  /// @brief GdsStruct の追加
-  /// @param[in] date 2つの日時の配列
-  /// @param[in] strname 構造名
-  void
-  add_struct(GdsDate* date,
-	     GdsString* strname);
+  /// @brief PATH 以降の読み込み
+  ///
+  /// エラーが起きたら false を返す．
+  /// 結果は mCurElement に格納される．
+  bool
+  read_path();
 
-  /// @brief GdsAref の作成
-  /// @param[in] elflags ELFLAGS の値
-  /// @param[in] plex PLEX の値
-  /// @param[in] strname 構造名
-  /// @param[in] strans STRANS の値
-  /// @param[in] colrow 列と行の数
-  /// @param[in] xy 座標
-  void
-  add_aref(ymuint16 elflags,
-	   ymint32 plex,
-	   GdsString* strname,
-	   GdsStrans* strans,
-	   ymuint32 colrow,
-	   GdsXY* xy);
+  /// @brief SREF 以降の読み込み
+  ///
+  /// エラーが起きたら false を返す．
+  /// 結果は mCurElement に格納される．
+  bool
+  read_sref();
 
-  /// @brief GdsBoundary の作成
-  /// @param[in] elflags ELFLAGS の値
-  /// @param[in] plex PLEX の値
-  /// @param[in] layer LAYER の値
-  /// @param[in] datatype DATATYPE の値
-  /// @param[in] xy XY の値
-  void
-  add_boundary(ymuint16 elflags,
-	       ymint32 plex,
-	       ymint16 layer,
-	       ymint16 datatype,
-	       GdsXY* xy);
+  /// @brief AREF 以降の読み込み
+  ///
+  /// エラーが起きたら false を返す．
+  /// 結果は mCurElement に格納される．
+  bool
+  read_aref();
 
-  /// @brief GdsBox の作成
-  /// @param[in] elflags ELFLAGS の値
-  /// @param[in] plex PLEX の値
-  /// @param[in] layer LAYER の値
-  /// @param[in] boxtype BOXTYPE の値
-  /// @param[in] xy XY の値
-  void
-  add_box(ymuint16 elflags,
-	  ymint32 plex,
-	  ymint16 layer,
-	  ymint16 boxtype,
-	  GdsXY* xy);
+  /// @brief TEXT 以降の読み込み
+  ///
+  /// エラーが起きたら false を返す．
+  /// 結果は mCurElement に格納される．
+  bool
+  read_text();
 
-  /// @brief GdsNode の作成
-  /// @param[in] elflags ELFLAGS の値
-  /// @param[in] plex PLEX の値
-  /// @param[in] layer LAYER の値
-  /// @param[in] nodetype DATATYPE の値
-  /// @param[in] xy XY の値
-  void
-  add_node(ymuint16 elflags,
-	   ymint32 plex,
-	   ymint16 layer,
-	   ymint16 nodetype,
-	   GdsXY* xy);
+  /// @brief NODE 以降の読み込み
+  ///
+  /// エラーが起きたら false を返す．
+  /// 結果は mCurElement に格納される．
+  bool
+  read_node();
 
-  /// @brief GdsPath の作成
-  /// @param[in] elflags ELFLAGS の値
-  /// @param[in] plex PLEX の値
-  /// @param[in] layer LAYER の値
-  /// @param[in] datatype DATATYPE の値
-  /// @param[in] pathtype PATYTYPE の値
-  /// @param[in] width WIDTH の値
-  /// @param[in] bgn_extn BGNEXTN の値
-  /// @param[in] end_extn ENDEXTN の値
-  /// @param[in] xy XY の値
-  void
-  add_path(ymuint16 elflags,
-	   ymint32 plex,
-	   ymint16 layer,
-	   ymint16 datatype,
-	   ymint16 pathtype,
-	   ymint32 width,
-	   ymint32 bgn_extn,
-	   ymint32 end_extn,
-	   GdsXY* xy);
+  /// @brief BOX 以降の読み込み
+  ///
+  /// エラーが起きたら false を返す．
+  /// 結果は mCurElement に格納される．
+  bool
+  read_box();
 
-  /// @brief GdsSref の作成
-  /// @param[in] elflags ELFLAGS の値
-  /// @param[in] plex PLEX の値
-  /// @param[in] strname 構造名
-  /// @param[in] strans STRANS の値
-  /// @param[in] xy 座標
-  void
-  add_sref(ymuint16 elflags,
-	   ymint32 plex,
-	   GdsString* strname,
-	   GdsStrans* strans,
-	   GdsXY* xy);
-
-  /// @brief GdsText の作成
-  /// @param[in] elflags ELFLAGS の値
-  /// @param[in] plex PLEX の値
-  /// @param[in] layer LAYER の値
-  /// @param[in] texttype TEXTTYPE の値
-  /// @param[in] presentation PRESENTATION の値
-  /// @param[in] pathtype PATHTYPE の値
-  /// @param[in] width WIDTH の値
-  /// @param[in] strans STRANS の値
-  /// @param[in] xy XY座標
-  /// @param[in] body 本体の文字列
-  void
-  add_text(ymuint16 elflags,
-	   ymint32 plex,
-	   ymint16 layer,
-	   ymint16 texttype,
-	   ymuint16 presentation,
-	   ymint16 pathtype,
-	   ymint32 width,
-	   GdsStrans* strans,
-	   GdsXY* xy,
-	   GdsString* body);
-
-  /// @brief GdsStrans の作成
-  GdsStrans*
-  new_strans(ymuint flags,
-	     double mag,
-	     double angle);
+  /// @brief STRANS 以降の読み込み
+  /// @param[out] strans 結果を格納する変数
+  ///
+  /// エラーが起きたら false を返す．
+  bool
+  read_strans(GdsStrans*& strans);
 
   /// @brief GdsElement (の派生要素)の追加
   void
