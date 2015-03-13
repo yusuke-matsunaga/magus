c3ce62e136c94ff770f50b9b48dbc9e7f013e55b
Yusuke Matsunaga <yusuke_matsunaga@ieee.org>
2015-03-13 23:05:44 +0900

    ﻿libym_utils/graph: McNodeHeap と MisNodeHeap の共通部分を NodeHeap にした．

--- libraries/libym_utils/src/graph/independent_set.cc
+++ libraries/libym_utils/src/graph/independent_set.cc
@@ -8,12 +8,59 @@
 
 
 #include "YmUtils/Graph.h"
-#include "MisNode.h"
-#include "MisNodeHeap.h"
+#include "Node.h"
+#include "NodeHeap.h"
 #include "YmUtils/SimpleAlloc.h"
 
 
 BEGIN_NAMESPACE_YM
+
+BEGIN_NONAMESPACE
+
+class IndepNodeHeap :
+  public NodeHeap
+{
+public:
+
+  /// @brief コンストラクタ
+  /// @param[in] num ノード数
+  IndepNodeHeap(ymuint num);
+
+
+private:
+
+  /// @brief 比較関数
+  /// @param[in] node1, node2 対象のノード
+  /// @retval 負の値 node1 が node2 より前にある．
+  /// @retval 0 node1 と node2 は同じ
+  /// @retval 正の値 node1 が node2 より後ろにある．
+  virtual
+  int
+  compare(Node* node1,
+	  Node* node2);
+
+};
+
+// @brief コンストラクタ
+// @param[in] num ノード数
+IndepNodeHeap::IndepNodeHeap(ymuint num) :
+  NodeHeap(num)
+{
+}
+
+// @brief 比較関数
+// @param[in] node1, node2 対象のノード
+// @retval 負の値 node1 が node2 より前にある．
+// @retval 0 node1 と node2 は同じ
+// @retval 正の値 node1 が node2 より後ろにある．
+int
+IndepNodeHeap::compare(Node* node1,
+		       Node* node2)
+{
+  return node1->adj_num() - node2->adj_num();
+}
+
+END_NONAMESPACE
 
 // @brief 独立集合を求める．
 // @param[in] graph 対象のグラフ
@@ -30,14 +77,14 @@ independent_set(const Graph& graph,
     return 0;
   }
 
-  // 各ノードに対応する MisNode というオブジェクトを作る．
+  // 各ノードに対応する Node というオブジェクトを作る．
   // ndoe_array[row_pos] に row_pos の行の Node が入る．
   // top から Node::mNext を使ってリンクとリストを作る．
   SimpleAlloc alloc;
-  MisNodeHeap node_heap(node_num);
+  IndepNodeHeap node_heap(node_num);
 
   for (ymuint i = 0; i < node_num; ++ i) {
-    MisNode* node = node_heap.node(i);
+    Node* node = node_heap.node(i);
     node->set(i);
   }
 
@@ -51,13 +98,13 @@ independent_set(const Graph& graph,
     link_array[id2].push_back(id1);
   }
   for (ymuint i = 0; i < node_num; ++ i) {
-    MisNode* node1 = node_heap.node(i);
+    Node* node1 = node_heap.node(i);
     const vector<ymuint>& link_list = link_array[i];
     ymuint link_num = link_list.size();
-    void* p = alloc.get_memory(sizeof(MisNode*) * link_num);
-    MisNode** adj_link = new (p) MisNode*[link_num];
+    void* p = alloc.get_memory(sizeof(Node*) * link_num);
+    Node** adj_link = new (p) Node*[link_num];
     for (ymuint j = 0; j < link_num; ++ j) {
-      MisNode* node2 = node_heap.node(link_list[j]);
+      Node* node2 = node_heap.node(link_list[j]);
       adj_link[j] = node2;
     }
     node1->set_adj_link(link_num, adj_link);
@@ -65,19 +112,19 @@ independent_set(const Graph& graph,
     node_heap.put_node(node1);
   }
 
-  // 未処理の MisNode のうち Node::adj_num() が最小のものを取り出し，解に加える．
+  // 未処理の Node のうち Node::adj_num() が最小のものを取り出し，解に加える．
   while ( !node_heap.empty() ) {
-    MisNode* best_node = node_heap.get_min();
+    Node* best_node = node_heap.get_min();
     node_set.push_back(best_node->id());
 
     // best_node に隣接しているノードも処理済みとする．
     for (ymuint i = 0; i < best_node->adj_size(); ++ i) {
-      MisNode* node2 = best_node->adj_node(i);
+      Node* node2 = best_node->adj_node(i);
       if ( !node2->deleted() ) {
 	node_heap.delete_node(node2);
 	// さらにこのノードに隣接しているノードの mNum を減らす．
 	for (ymuint j = 0; j < node2->adj_size(); ++ j) {
-	  MisNode* node3 = node2->adj_node(j);
+	  Node* node3 = node2->adj_node(j);
 	  if ( !node3->deleted() ) {
 	    node3->dec_adj_num();
 	    node_heap.update(node3);
--- libraries/libym_utils/src/graph/max_clique.cc
+++ libraries/libym_utils/src/graph/max_clique.cc
@@ -8,12 +8,59 @@
 
 
 #include "YmUtils/Graph.h"
-#include "McNode.h"
-#include "McNodeHeap.h"
+#include "Node.h"
+#include "NodeHeap.h"
 #include "YmUtils/SimpleAlloc.h"
 
 
 BEGIN_NAMESPACE_YM
+
+BEGIN_NONAMESPACE
+
+class MaxCliqueNodeHeap :
+  public NodeHeap
+{
+public:
+
+  /// @brief コンストラクタ
+  /// @param[in] num ノード数
+  MaxCliqueNodeHeap(ymuint num);
+
+
+private:
+
+  /// @brief 比較関数
+  /// @param[in] node1, node2 対象のノード
+  /// @retval 負の値 node1 が node2 より前にある．
+  /// @retval 0 node1 と node2 は同じ
+  /// @retval 正の値 node1 が node2 より後ろにある．
+  virtual
+  int
+  compare(Node* node1,
+	  Node* node2);
+
+};
+
+// @brief コンストラクタ
+// @param[in] num ノード数
+MaxCliqueNodeHeap::MaxCliqueNodeHeap(ymuint num) :
+  NodeHeap(num)
+{
+}
+
+// @brief 比較関数
+// @param[in] node1, node2 対象のノード
+// @retval 負の値 node1 が node2 より前にある．
+// @retval 0 node1 と node2 は同じ
+// @retval 正の値 node1 が node2 より後ろにある．
+int
+MaxCliqueNodeHeap::compare(Node* node1,
+			   Node* node2)
+{
+  return node2->adj_num() - node1->adj_num();
+}
+
+END_NONAMESPACE
 
 // @brief 最大クリークを求める．
 // @param[in] graph 対象のグラフ
@@ -30,18 +77,15 @@ max_clique(const Graph& graph,
     return 0;
   }
 
-  // 各ノードに対応する McNode というオブジェクトを作る．
+  // 各ノードに対応する Node というオブジェクトを作る．
   // ndoe_array[row_pos] に row_pos の行の Node が入る．
   // top から Node::mNext を使ってリンクとリストを作る．
   SimpleAlloc alloc;
-  McNodeHeap node_heap(node_num);
+  MaxCliqueNodeHeap node_heap(node_num);
 
-  void* p = alloc.get_memory(sizeof(McNode*) * node_num);
-  McNode** node_array = new (p) McNode*[node_num];
   for (ymuint i = 0; i < node_num; ++ i) {
-    McNode* node = node_heap.node(i);
+    Node* node = node_heap.node(i);
     node->set(i);
-    node_array[i] = node;
   }
 
   // 隣接するノードの情報を link_array に入れる．
@@ -54,13 +98,13 @@ max_clique(const Graph& graph,
     link_array[id2].push_back(id1);
   }
   for (ymuint i = 0; i < node_num; ++ i) {
-    McNode* node1 = node_array[i];
+    Node* node1 = node_heap.node(i);
     const vector<ymuint>& link_list = link_array[i];
     ymuint link_num = link_list.size();
-    void* p = alloc.get_memory(sizeof(McNode*) * link_num);
-    McNode** adj_link = new (p) McNode*[link_num];
+    void* p = alloc.get_memory(sizeof(Node*) * link_num);
+    Node** adj_link = new (p) Node*[link_num];
     for (ymuint j = 0; j < link_num; ++ j) {
-      McNode* node2 = node_array[link_list[j]];
+      Node* node2 = node_heap.node(link_list[j]);
       adj_link[j] = node2;
     }
     node1->set_adj_link(link_num, adj_link);
@@ -68,19 +112,19 @@ max_clique(const Graph& graph,
     node_heap.put_node(node1);
   }
 
-  // 未処理の McNode のうち Node::adj_num() が最大のものを取り出し，解に加える．
+  // 未処理の Node のうち Node::adj_num() が最大のものを取り出し，解に加える．
   while ( !node_heap.empty() ) {
-    McNode* best_node = node_heap.get_min();
+    Node* best_node = node_heap.get_min();
     node_set.push_back(best_node->id());
 
     for (ymuint i = 0; i < best_node->adj_size(); ++ i) {
       // best_node に隣接していないノードも処理済みとする．
-      McNode* node2 = best_node->adj_node(i);
+      Node* node2 = best_node->adj_node(i);
       if ( !node2->deleted() ) {
 	node_heap.delete_node(node2);
 	// さらにこのノードに隣接しているノードの mNum を減らす．
 	for (ymuint j = 0; j < node2->adj_size(); ++ j) {
-	  McNode* node3 = node2->adj_node(j);
+	  Node* node3 = node2->adj_node(j);
 	  if ( !node3->deleted() ) {
 	    node3->dec_adj_num();
 	    node_heap.update(node3);
