621afbef1bd2d278a5c328e6fe8d7e8c9c954ddc
Yusuke Matsunaga <yusuke_matsunaga@ieee.org>
2015-06-03 22:00:55 +0900

    ﻿nlink: NlSolverDfs 作成中

---
+++ programs/nlink/include/NlSolverDfs.h
@@ -0,0 +1,104 @@
+#ifndef NLSOLVERDFS_H
+#define NLSOLVERDFS_H
+
+/// @file NlSolverDfs.h
+/// @brief NlSolverDfs のヘッダファイル
+/// @author Yusuke Matsunaga (松永 裕介)
+///
+/// Copyright (C) 2015 Yusuke Matsunaga
+/// All rights reserved.
+
+
+#include "NlSolver.h"
+#include "YmLogic/SatSolver.h"
+
+
+BEGIN_NAMESPACE_YM_NLINK
+
+class FrontierInfo;
+class Ban;
+
+//////////////////////////////////////////////////////////////////////
+/// @class NlSolverDfs NlSolverDfs.h "NlSolverDfs.h"
+/// @brief number link を解くためのクラス
+//////////////////////////////////////////////////////////////////////
+class NlSolverDfs :
+  public NlSolver
+{
+public:
+
+  /// @brief コンストラクタ
+  NlSolverDfs();
+
+  /// @brief デストラクタ
+  virtual
+  ~NlSolverDfs();
+
+
+public:
+  //////////////////////////////////////////////////////////////////////
+  // 外部インターフェイス
+  //////////////////////////////////////////////////////////////////////
+
+  /// @brief 問題を解く
+  /// @param[in] problem 問題
+  /// @param[in] verbose verbose フラグ
+  /// @param[out] solution 解
+  virtual
+  void
+  solve(const NlProblem& problem,
+	bool verbose,
+	NlSolution& solution);
+
+
+private:
+  //////////////////////////////////////////////////////////////////////
+  // 内部で用いられる関数
+  //////////////////////////////////////////////////////////////////////
+
+  /// @brief 枝の順序付けを行う．
+  void
+  ordering(const NlGraph& graph,
+	   vector<const NlEdge*>& edge_list);
+
+  /// @brief 探索を行う．
+  void
+  dfs_recur(const Ban& ban,
+	    cosnt NlGraph& graph);
+
+  /// @brief フロンティアの更新を行う．
+  /// @param[inout] frontier_nodes フロンティアノードのリスト
+  /// @param[in] edge 新しく加わる枝
+  /// @param[in] del_list 削除されるノードのフロンティアリスト上の位置
+  /// @param[out] pos1, pos2 枝の両端のノードの位置
+  ///
+  /// pos1, pos2 は該当のノードがない場合には -1 を返す．
+  void
+  calc_frontier(vector<const NlNode*>& frontier_nodes,
+		const NlEdge* edge,
+		ymuint max_node_id,
+		vector<ymuint>& del_list,
+		int& pos1,
+		int& pos2);
+
+  /// @brief フロンティアの更新を行う．
+  bool
+  update_frontier(FrontierInfo& fr,
+		  const NlEdge* edge,
+		  bool selected,
+		  const NlGraph& graph);
+
+
+private:
+  //////////////////////////////////////////////////////////////////////
+  // データメンバ
+  //////////////////////////////////////////////////////////////////////
+
+  // 枝の順序を記憶する配列
+  vector<ymuint> mEdgeOrder;
+
+};
+
+END_NAMESPACE_YM_NLINK
+
+#endif // NLSOLVERDFS_H
