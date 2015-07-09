8352ca606e1021d022cc69919994038022ee4c4c
Yusuke Matsunaga <yusuke_matsunaga@ieee.org>
2015-07-09 14:38:03 +0900

    ﻿CMakeLists.txt を変更
    * ym_add_library の定義を別ファイルにした．

---
+++ cmake/ym_add_library.cmake
@@ -0,0 +1,68 @@
+﻿
+# release, relwithdeb, debug ターゲットライブラリの設定
+function (ym_add_library)
+  foreach ( pos RANGE 0 ${ARGC} )
+    if ( ${pos} EQUAL ${ARGC} )
+      break()
+    endif ()
+    list (GET ARGV ${pos} argv)
+    if ( ${pos} EQUAL 0 )
+      set (_target_name ${argv})
+    elseif ( ${pos} EQUAL 1 )
+      if ( ${argv} STREQUAL "SHARED" )
+	set (_option "SHARED")
+      elseif ( ${argv} STREQUAL "STATIC" )
+	set (_option "STATIC")
+      else ()
+	list (APPEND _sources ${argv})
+      endif ()
+    else ()
+      list (APPEND _sources ${argv})
+    endif ()
+  endforeach ()
+
+  # release モードのターゲット
+  add_library (${_target_name}
+    ${_option}
+    ${_sources}
+    )
+
+  target_compile_options (${_target_name}
+    PRIVATE "-O3"
+    )
+
+  target_compile_definitions (${_target_name}
+    PRIVATE "-DNDEBUG"
+    )
+
+  # release with debug info モードのターゲット
+  set (_target_name_p ${_target_name}_p)
+  add_library (${_target_name_p}
+    ${_option}
+    ${_sources}
+    )
+
+  target_compile_options (${_target_name_p}
+    PRIVATE "-O2" "-g"
+    )
+
+  target_compile_definitions (${_target_name_p}
+    PRIVATE "-DNDEBUG"
+    )
+
+  # debug モードのターゲット
+  set (_target_name_d ${_target_name}_d)
+  add_library (${_target_name_d}
+    ${_option}
+    ${_sources}
+    )
+
+  target_compile_options (${_target_name_d}
+    PRIVATE "-g"
+    )
+
+  target_compile_definitions (${_target_name_d}
+    PRIVATE
+    )
+
+endfunction ()
