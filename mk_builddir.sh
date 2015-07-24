ac0c62b75adde80c8096375e08c32ac440692a70
Yusuke Matsunaga <yusuke_matsunaga@ieee.org>
2015-07-25 06:29:51 +0900

    ﻿mk_builddir.sh, etc/do_cmake.sh.in の細かな修正
    * ymtclpp と同様の修正

--- mk_builddir.sh
+++ mk_builddir.sh
@@ -1,19 +1,22 @@
 #! /bin/sh
-
+#
 # mk_builddir.sh
 #
 # コンパイル用のディレクトリを作るシェルスクリプト
 #
-# Copyright (C) 2015 Yusuke Matsunaga
+# Copyright (C) 2015 Yusuke Matsunaga (松永 裕介)
 # All rights reserved
 
+# コマンドの定義
+# 環境変数 SED をセットしておけばその値が使われる．
+# そうでなければデフォルトの sed が使われる．
+echo ${SED:="sed"} > /dev/null
 
 # このファイルのあるディレクトリを srcdir にセットする．
 basedir=`dirname $0`
 srcdir=`cd $basedir; pwd`
 
-ymtoolsdir=NONE
-ymtclppdir=NONE
+# オプション解析用のループ
 while [ $# -ge 4 ] ; do
     case $1 in
 	"--ymtools_dir")
@@ -28,6 +31,7 @@ while [ $# -ge 4 ] ; do
     esac
 done
 
+# 引数の数が異なっていたら usage を表示して終わる．
 if [ $# -ne 2 ]; then
     echo "USAGE mk_builddir.sh [--ymtools_dir <ymtools-dir>] [--ymtclpp_dir <ymtclpp-dir>] <compiledir> <installdir>"
     exit 1
@@ -39,20 +43,17 @@ builddir=$1
 # インストール先のディレクトリ名
 installdir=$2
 
-if [ $ymtoolsdir = NONE ]; then
-    ymtoolsdir=$installdir
-fi
-
-if [ $ymtclppdir = NONE ]; then
-    ymtclppdir=$installdir
-fi
-
+# ディレクトリ名を表示して確認を求める．
 echo "****"
 echo "source  directory: $srcdir"
 echo "build   directory: $builddir"
 echo "install directory: $installdir"
-echo "ymtools directory: $ymtoolsdir"
-echo "ymtclpp directory: $ymtclppdir"
+if [ "x$ymtoolsdir" != x ]; then
+    echo "ymtools directory: $ymtoolsdir"
+fi
+if [ "x$ymtclppdir" != x ]; then
+    echo "ymtclpp directory: $ymtclppdir"
+fi
 echo "****"
 echo -n "continue ? (yes/no)"
 while read confirmation; do
@@ -71,16 +72,17 @@ done
 # ビルドディレクトリはなければ作る．
 test -d $builddir || mkdir -p $builddir
 
-# do_cmake ファイルを作る．
+# do_cmake.sh ファイルを作る．
 do_cmake="do_cmake.sh"
-sed -e s!__YM_SRC_DIRECTORY__!$srcdir! \
-    -e s!__YM_INSTALL_DIRECTORY__!$installdir! \
-    -e s!__YM_YMTOOLS_DIRECTORY__!$ymtoolsdir! \
-    -e s!__YM_YMTCLPP_DIRECTORY__!$ymtclppdir! \
-    $srcdir/etc/${do_cmake}.in > $builddir/${do_cmake}
-chmod +x $builddir/${do_cmake}
+$SED -e s!___SRC_DIR___!$srcdir! \
+     -e s!___INSTALL_DIR___!$installdir! \
+     -e s!___YMTOOLS_DIR___!$ymtoolsdir! \
+     -e s!___YMTCLPP_DIR___!$ymtclppdir! \
+     $srcdir/etc/${do_cmake}.in > $builddir/$do_cmake
+chmod +x $builddir/$do_cmake
 
 # do_cmake.sh を実行する．
-(cd $builddir && ./${do_cmake})
+echo "running cmake"
+(cd $builddir && ./$do_cmake)
 
 echo "  done"
