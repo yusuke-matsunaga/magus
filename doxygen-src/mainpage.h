dc5e5f05f5668e537ec4632cb6712157d1a4d676
Yusuke Matsunaga <yusuke_matsunaga@ieee.org>
2018-10-12 12:59:27 +0900

    ym-common の変更に合わせた修正
    * CMakeLists.txt を新規マクロを使って簡略化した．
    * doxygen-src/Doxyfile.in を etc/ に移動
    * doxygen-src/CMakeLists.txt の内容を CMakeLists.tx に移動
    * doxygen-src/mainpage.h を追加

---
+++ doxygen-src/mainpage.h
@@ -0,0 +1,19 @@
+﻿/// @file mainpage.h
+/// @brief Magus のメインページ
+/// @author Yusuke Matsunaga (松永 裕介)
+///
+/// Copyright (C) 2018 Yusuke Matsunaga
+/// All rights reserved.
+
+
+/// @mainpage
+///
+/// Magus は論理合成・論理検証プログラムです．
+///
+/// 現在は tclsh の拡張版の magus_tclsh と Python ライブラリ
+/// の２種類があります．
+///
+/// tclsh 版は今後は開発予定はありません．
+/// すべて Python 版に移行します．
+
+#error "don't include me!"
