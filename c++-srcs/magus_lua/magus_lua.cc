403a83d0c4216fcea125fc4ba8dfa1d11c86979c
Yusuke Matsunaga <yusuke_matsunaga@ieee.org>
2022-07-06 15:28:07 +0900

    Luapp 関係の CMakeLists.txt の記述を変更した．
    * 基本的に ${LUA_LIBRARIES} を明示的に書く必要はなくなった．

--- c++-srcs/magus_lua/magus_lua.cc
+++ c++-srcs/magus_lua/magus_lua.cc
@@ -6,27 +6,7 @@
 /// Copyright (C) 2022 Yusuke Matsunaga
 /// All rights reserved.
 
-#include <unistd.h> // for getopt()
-#include <libgen.h> // for basename()
-#include <stdio.h>
-#include <ym/ReadLine.h>
 #include "LuaMagus.h"
-
-
-BEGIN_NONAMESPACE
-
-void
-usage(
-  char** argv
-)
-{
-  using namespace std;
-
-  cerr << "Usage: " << basename(argv[0])
-       << " <script-file>" << endl;
-}
-
-END_NONAMESPACE
 
 
 int
@@ -35,58 +15,10 @@ main(
   char** argv
 )
 {
-  using namespace std;
-  using YM_NAMESPACE::ReadLine;
-  using MAGUS_NAMESPACE::LuaMagus;
-
-  LuaMagus lua;
+  MAGUS_NAMESPACE::LuaMagus lua;
 
   lua.L_openlibs();
   lua.open_Magus();
 
-#if 0
-  if ( argc == 1 ) {
-    // インタラクティブモード
-    const char* prompt{"% "};
-    for ( ; ; ) {
-      string linebuf;
-      if ( !ReadLine::get_line(prompt, linebuf) ) {
-	break;
-      }
-      int err = lua.L_loadstring(linebuf.c_str()) || lua.pcall(0, 0, 0);
-      if ( err ) {
-	cerr << lua.to_string(-1) << endl;
-	lua.pop(1);
-      }
-    }
-  }
-  else if ( argc >= 2 ) {
-    // 引数を lua のグローバル変数 "arg" にセットする．
-    lua.create_table(argc, 0);
-    for ( int i = 0; i < argc; ++ i ) {
-      lua.push_string(argv[i]);
-      lua.set_table(-2, i);
-    }
-    lua.set_global("arg");
-    // 先頭の引数をスクリプトファイルとみなして実行する．
-    char* init_file = argv[1];
-    string script{argv[1]};
-    // スクリプトファイルの場所を変数 "script_dir" にセットする．
-    char* dir_name = dirname(init_file);
-    lua.push_string(dir_name);
-    lua.set_global("script_dir");
-    if ( script != string{} && lua.L_dofile(script.c_str()) ) {
-      cerr << lua.to_string(-1) << endl;
-      return 1;
-    }
-  }
-  else {
-    usage(argv);
-    return 1;
-  }
-
-  return 0;
-#else
   return lua.main_loop(argc, argv);
-#endif
 }
